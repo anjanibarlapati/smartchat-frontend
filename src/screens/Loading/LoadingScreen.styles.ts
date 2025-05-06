b1764ad8a0555a24eea907117ee8b3edb3f15316
SoumyaKatta <soumya.katta@everest.engineering>
2025-05-06 10:42:02 +0530

    Update style file

---
+++ src/screens/Loading/LoadingScreen.styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+export const loadingImage = StyleSheet.create({
+  container: {
+    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
+  loadingImage: {
+    height: 200,
+    aspectRatio: 1,
+  },
+});
