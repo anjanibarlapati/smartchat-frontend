da22d1f0107dc54a1f1cdca0d3301f3afb09c4f3
SoumyaKatta <soumya.katta@everest.engineering>
2025-05-06 10:40:42 +0530

    Write test case to check whether App component is renders or not

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 import App from './App.tsx';
 
-
 const renderApp = () => {
-  return render(
-    <App/>
-  );
+  return render(<App />);
 };
 
 test('renders welcome message correctly', async () => {
-  const { getByText } = renderApp();
-  expect(getByText(/welcome to smartchat/i)).toBeTruthy();
+  const {getByTestId} = renderApp();
+  expect(getByTestId('animation')).toBeTruthy();
 });
