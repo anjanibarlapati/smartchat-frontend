9ddbada18e7942cdf484b3329a6f58d5c601e5c6
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-07 10:07:14 +0530

    Add splash screen hide function to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { View} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import {View} from 'react-native';
 import {app} from './App.ts';
 import WelcomeScreen from '../screens/Welcome/Welcome.tsx';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View style={app.container}>
       <WelcomeScreen />
