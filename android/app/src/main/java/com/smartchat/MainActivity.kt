1889ee15adfeee15d608f61179a79cdd2fde9b1e
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-07 10:04:57 +0530

    Add splash screen Android and changed some files

--- android/app/src/main/java/com/smartchat/MainActivity.kt
+++ android/app/src/main/java/com/smartchat/MainActivity.kt
@@ -4,9 +4,14 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
-
+ override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)  // First, call the superclass method
+        SplashScreen.show(this)  // Then show the splash screen
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
