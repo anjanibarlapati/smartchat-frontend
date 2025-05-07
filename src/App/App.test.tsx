40490118d8a6b125b3f3458fa2371a44ed2144f7
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-07 10:06:18 +0530

    Implement tests for the Splash-screen

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,13 +1,22 @@
 import App from './App.tsx';
 import React from 'react';
 import {render} from '@testing-library/react-native';
-
+import SplashScreen from 'react-native-splash-screen';
 const renderApp = () => {
   return render(<App />);
 };
 
+jest.mock('react-native-splash-screen', () => ({
+  hide: jest.fn(),
+}));
+
 test('renders welcome screen correctly', async () => {
-  const { getByText } = renderApp();
+  const {getByText} = renderApp();
   const smartchatText = getByText('SmartChat');
   expect(smartchatText).toBeTruthy();
+});
+
+test('calls SplashScreen.hide on mount',  () => {
+  render(<App />);
+    expect(SplashScreen.hide).toHaveBeenCalled();
 });
