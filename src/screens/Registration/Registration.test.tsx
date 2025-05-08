be4b646164cb478ef43d9d2243956e169d7d2430
varun-martha <varunkumar.martha@everest.engineering>
2025-05-08 16:20:25 +0530

    Add test cases for handleRegister for Registration screen

--- src/screens/Registration/Registration.test.tsx
+++ src/screens/Registration/Registration.test.tsx
@@ -1,14 +1,36 @@
-import {fireEvent, render, waitFor} from '@testing-library/react-native';
-import {Provider} from 'react-redux';
-import store from '../../redux/store';
+import { Alert } from 'react-native';
+import {act, fireEvent, render, waitFor} from '@testing-library/react-native';
 import Registration from './Registration';
+import * as RegistrationHandler from './Registration.handler';
+
+jest.mock('../../utils/openCamera', () => ({
+  openCamera: jest.fn(),
+}));
+
+jest.mock('../../utils/openPhotoLibrary', () => ({
+  openPhotoLibrary: jest.fn(),
+}));
 
 describe('Registration Screen check', () => {
+
+  let mockRegister: jest.SpyInstance;
+
+  beforeAll(() => {
+    mockRegister = jest.spyOn(RegistrationHandler, 'register');
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.spyOn(Alert, 'alert').mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    mockRegister.mockRestore();
+  });
+
   it('renders the registration screen correctly', () => {
     const {getByPlaceholderText, getByText, getByLabelText} = render(
-      <Provider store={store}>
-        <Registration />
-      </Provider>,
+      <Registration />
     );
     const image = getByLabelText('profile-image');
     expect(image).toBeTruthy();
@@ -22,39 +44,34 @@ describe('Registration Screen check', () => {
   });
   it('shows validation errors when fields are empty', async () => {
     const {getByText, queryByText} = render(
-      <Provider store={store}>
-        <Registration />
-      </Provider>,
+      <Registration />
     );
     fireEvent.press(getByText('Register'));
     await waitFor(() => {
       expect(queryByText('First name is required')).toBeTruthy();
-      expect(queryByText(' Last name is required')).toBeTruthy();
+      expect(queryByText('Last name is required')).toBeTruthy();
       expect(queryByText('Mobile number is required')).toBeTruthy();
       expect(queryByText('Password is required')).toBeTruthy();
     });
   });
   it('shows error when passwords do not match', async () => {
     const {getByPlaceholderText, getByText, queryByText} = render(
-      <Provider store={store}>
-        <Registration />
-      </Provider>,
+      <Registration />
     );
     fireEvent.changeText(getByPlaceholderText('First Name *'), 'Mamatha');
     fireEvent.changeText(getByPlaceholderText('Last Name *'), 'Niya;');
     fireEvent.changeText(getByPlaceholderText('Mobile Number *'), '1234567890');
     fireEvent.changeText(getByPlaceholderText('Password *'), 'pass123');
     fireEvent.changeText(getByPlaceholderText('Confirm Password *'), '123');
-    fireEvent.press(getByText('Register'));
-    await waitFor(() => {
+    await act(async ()=> {
+      fireEvent.press(getByText('Register'));
+    });    await waitFor(() => {
       expect(queryByText('Passwords do not match')).toBeTruthy();
     });
   });
   it('shows error for invalid email address', async () => {
     const {getByPlaceholderText, getByText, queryByText} = render(
-      <Provider store={store}>
-        <Registration />
-      </Provider>,
+      <Registration />
     );
 
     fireEvent.changeText(getByPlaceholderText('First Name *'), 'Mamatha');
@@ -67,8 +84,9 @@ describe('Registration Screen check', () => {
       'password123',
     );
 
-    fireEvent.press(getByText('Register'));
-
+    await act(async ()=> {
+      fireEvent.press(getByText('Register'));
+    });
     await waitFor(() => {
       expect(queryByText('Invalid email address')).toBeTruthy();
     });
@@ -76,9 +94,7 @@ describe('Registration Screen check', () => {
 
   it('submits the form successfully when all fields are valid', async () => {
     const {getByPlaceholderText, getByText} = render(
-      <Provider store={store}>
-        <Registration />
-      </Provider>,
+      <Registration />
     );
     fireEvent.changeText(getByPlaceholderText('First Name *'), 'Anjani');
     fireEvent.changeText(getByPlaceholderText('Last Name *'), 'Barlapati');
@@ -86,6 +102,81 @@ describe('Registration Screen check', () => {
     fireEvent.changeText(getByPlaceholderText('Mobile Number *'), '9876543210');
     fireEvent.changeText(getByPlaceholderText('Password *'), '1234');
     fireEvent.changeText(getByPlaceholderText('Confirm Password *'), '1234');
-    fireEvent.press(getByText('Register'));
+    await act(async ()=> {
+        fireEvent.press(getByText('Register'));
+      });
+    });
+
+  it('should give an alert on successful registration with message', async () => {
+    const response = {
+      ok: true,
+      json: async () => ({ message: 'Registered Successfully' }),
+    };
+    mockRegister.mockResolvedValue(response);
+
+    const { getByPlaceholderText, getByText } = render(
+      <Registration />
+    );
+
+    fireEvent.changeText(getByPlaceholderText('First Name *'), 'Varun');
+    fireEvent.changeText(getByPlaceholderText('Last Name *'), 'Kumar');
+    fireEvent.changeText(getByPlaceholderText('Email'), 'varun@gmail.com');
+    fireEvent.changeText(getByPlaceholderText('Mobile Number *'), '1234567890');
+    fireEvent.changeText(getByPlaceholderText('Password *'), '1234');
+    fireEvent.changeText(getByPlaceholderText('Confirm Password *'), '1234');
+
+    await act(async ()=> {
+      fireEvent.press(getByText('Register'));
+    });
+    await waitFor(() => {
+      expect(Alert.alert).toHaveBeenCalledWith('User Registered Successfully!');
+    });
   });
+
+  it('should give an alert with error message if the API gives an error', async () => {
+    const response = {
+      ok: false,
+      json: async () => ({ message: 'User already exists' }),
+    };
+    mockRegister.mockResolvedValue(response);
+
+    const { getByPlaceholderText, getByText } = render(
+      <Registration />
+    );
+
+    fireEvent.changeText(getByPlaceholderText('First Name *'), 'Varun');
+    fireEvent.changeText(getByPlaceholderText('Last Name *'), 'Kumar');
+    fireEvent.changeText(getByPlaceholderText('Email'), 'varun@gmail.com');
+    fireEvent.changeText(getByPlaceholderText('Mobile Number *'), '1234567890');
+    fireEvent.changeText(getByPlaceholderText('Password *'), '1234');
+    fireEvent.changeText(getByPlaceholderText('Confirm Password *'), '1234');
+    await act(async ()=> {
+      fireEvent.press(getByText('Register'));
+    });
+    await waitFor(() => {
+      expect(Alert.alert).toHaveBeenCalledWith('User already exists');
+    });
+  });
+
+  it('should give an alert with Invalid error shows if API throws an error', async () => {
+    mockRegister.mockRejectedValue(new Error('Internal server error'));
+
+    const { getByPlaceholderText, getByText } = render(
+      <Registration />
+    );
+
+    fireEvent.changeText(getByPlaceholderText('First Name *'), 'Varun');
+    fireEvent.changeText(getByPlaceholderText('Last Name *'), 'Kumar');
+    fireEvent.changeText(getByPlaceholderText('Email'), 'varun@gmail.com');
+    fireEvent.changeText(getByPlaceholderText('Mobile Number *'), '5432123456');
+    fireEvent.changeText(getByPlaceholderText('Password *'), '1234');
+    fireEvent.changeText(getByPlaceholderText('Confirm Password *'), '1234');
+    await act(async ()=> {
+      fireEvent.press(getByText('Register'));
+    });
+    await waitFor(() => {
+      expect(Alert.alert).toHaveBeenCalledWith('Invalid error!');
+    });
+  });
+
 });
