57dae9d13ecb3d0e52139125dfd4cdc173a5e3cc
varun-martha <varunkumar.martha@everest.engineering>
2025-05-08 16:14:32 +0530

    Implement selectPicture, clickPicture and remove functions

--- src/components/ProfilePicturePickerModal/ProfilePicturePickerModal.tsx
+++ src/components/ProfilePicturePickerModal/ProfilePicturePickerModal.tsx
@@ -1,16 +1,65 @@
-import { Image, Modal, Pressable, Text, View } from 'react-native';
 import React from 'react';
+import { Alert, Image, Modal, Pressable, Text, View } from 'react-native';
 import { styles } from './ProfilePicturePickerModal.styles';
+import { UploadImage } from '../../types/UploadImage';
+import { openPhotoLibrary } from '../../utils/openPhotoLibrary';
+import { openCamera } from '../../utils/openCamera';
 
 
 interface ProfilePicturePickerModalProps {
     isEditingProfilePicture: boolean;
     close: () => void;
-    profilePicture: string,
+    profilePicture: UploadImage | null | string,
+    setProfilePic:  React.Dispatch<React.SetStateAction<string | UploadImage | null>>
     openedFrom: 'registration' | 'profile';
 }
 
 export function ProfilePicutrePickerModal(props: ProfilePicturePickerModalProps): React.JSX.Element{
+
+    const validFileTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+    const selectPicture = async() => {
+        const selectedPicture = await openPhotoLibrary();
+        if(selectedPicture === null) {
+            Alert.alert('You do not have permissions to select the picture');
+            return;
+        }
+        if(!validFileTypes.find(type => type === selectedPicture.mime)) {
+            Alert.alert('File should be JPG/PNG/JPEG format');
+            return;
+        }
+        props.close();
+        props.setProfilePic({
+            uri: selectedPicture.path,
+            type: selectedPicture.mime,
+            name: selectedPicture.filename,
+        } as UploadImage);
+    };
+
+    const removePicture = () => {
+        if(props.openedFrom === 'registration') {
+            props.setProfilePic(null);
+            props.close();
+        }
+    };
+
+    const capturePicture = async() => {
+        const selectedPicture = await openCamera();
+        if(selectedPicture === null) {
+            Alert.alert('You do not have permissions to select the picture');
+            return;
+        }
+        if(!validFileTypes.find(type => type === selectedPicture.mime)) {
+            Alert.alert('File should be JPG/PNG/JPEG format');
+            return;
+        }
+        props.close();
+        props.setProfilePic({
+            uri: selectedPicture.path,
+            type: selectedPicture.mime,
+            name: selectedPicture.filename,
+        } as UploadImage);
+    };
 
     return (
         <Modal visible={props.isEditingProfilePicture} transparent={true} animationType="slide">
@@ -24,21 +73,21 @@ export function ProfilePicutrePickerModal(props: ProfilePicturePickerModalProps)
                             <Text style={styles.text}>Profile Photo</Text>
                         </View>
                         <View style={styles.icons}>
-                            <View style={styles.iconContainer}>
+                            <Pressable style={styles.iconContainer} onPress={capturePicture}>
                                 <Image source={require('../../../assets/icons/camera-icon.png')} style={styles.icon} accessibilityLabel="camera-icon" />
                                 <Text style={styles.iconText}>Camera</Text>
-                            </View>
-                            <View style={styles.iconContainer}>
+                            </Pressable>
+                            <Pressable style={styles.iconContainer} onPress={selectPicture}>
                                 <Image source={require('../../../assets/icons/gallery-icon.png')} style={styles.icon} accessibilityLabel="gallery-icon" />
                                 <Text style={styles.iconText}>Gallery</Text>
-                            </View>
+                            </Pressable>
                             { props.profilePicture &&
-                                <View style={styles.iconContainer}>
+                                <Pressable style={styles.iconContainer} onPress={removePicture}>
                                     <Image source={require('../../../assets/icons/delete-icon.png')} style={styles.icon} accessibilityLabel="delete-icon" />
                                     <Text style={styles.iconText}>Remove</Text>
-                                </View>
+                                </Pressable>
                             }
-                    </View>
+                        </View>
                     </View>
                 </View>
         </Modal>
