26ee23aa915527ebd9997e433e3ddcd97a7418d9
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-10 16:12:27 +0530

    Modify styles of input field component

--- src/components/InputField/InputField.styles.ts
+++ src/components/InputField/InputField.styles.ts
@@ -8,7 +8,7 @@ export const styles = StyleSheet.create({
     input: {
       height: 40,
       borderColor: '#F2F2F2',
-      backgroundColor: '#F2F2F2',
+      backgroundColor: 'rgb(232, 232, 232)',
       borderWidth: 1,
       borderRadius: 5,
       paddingLeft: 10,
