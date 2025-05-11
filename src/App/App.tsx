b686cf36188ad1776f5a6fcca08cb15afb673894
Shailaja Nimmagari <admin@ShailajaCurious-Intern.local>
2025-05-11 18:16:15 +0530

    Implement multi language support in a useEffect in App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { AppNavigator } from '../navigations/AppNavigator.tsx';
 import { store } from '../redux/store.ts';
+import i18next from 'i18next';
+import * as RNLocalize from 'react-native-localize';
+import { resources } from '../i18n/i18n.config.ts';
+
 
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    const locales = RNLocalize.getLocales();
+    const languageTag =
+      locales.find((locale) => Object.keys(resources).includes(locale.languageCode))?.languageCode || 'en';
+      i18next.changeLanguage(languageTag);
+  }, []);
+
   return (
     <Provider store={store}>
       <AppNavigator/>
