d2a4e8081d10a541abbfbc02dd98a25e8eb19d4f
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-11 22:34:12 +0530

    Modify styling for App

--- src/App/App.styles.ts
+++ src/App/App.styles.ts
@@ -2,10 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export const app = StyleSheet.create({
    container:{
-       height:'100%',
-       flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center',
        backgroundColor:'white',
    },
    text: {
