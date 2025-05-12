49bb4df5cf5ae8d8a0e87a92a18466b84d280de7
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-12 19:13:54 +0530

    Add tests for Home Screen

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import { Provider } from 'react-redux';
-import { Home } from './Home';
-import { store } from '../../redux/store';
+import {fireEvent, render} from '@testing-library/react-native';
+import {Provider} from 'react-redux';
+import {Home} from './Home';
+import {store} from '../../redux/store';
+import {useNavigation} from '@react-navigation/native';
 
-
-function renderHomeScreen () {
-    return render(
-        <Provider store={store}>
-           <Home />
-        </Provider>
-    );
+function renderHomeScreen() {
+  return render(
+    <Provider store={store}>
+      <Home />
+    </Provider>,
+  );
 }
-
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: jest.fn(),
+}));
 describe('Home Screen', () => {
-  test('renders loading animation correctly', async () => {
+  const mockNavigate = jest.fn();
+  beforeEach(() => {
+    (useNavigation as jest.Mock).mockReturnValue({
+      navigate: mockNavigate,
+    });
+    jest.clearAllMocks();
+  });
+  test('should render start conversation text correctly', async () => {
     const {getByText} = renderHomeScreen();
-    expect(getByText(/welcome to home/i)).toBeTruthy();
+    expect(
+      getByText(/Start Conversations with your closed ones/i),
+    ).toBeTruthy();
+  });
+  test('Should render add contact button', () => {
+    const {getByLabelText} = renderHomeScreen();
+    expect(getByLabelText('addContact')).toBeTruthy();
+  });
+
+  test('Should navigate to ContactScreen on pressing add contact button', () => {
+    const {getByLabelText} = renderHomeScreen();
+    fireEvent.press(getByLabelText('addContact'));
+    expect(mockNavigate).toHaveBeenCalledWith('Contact');
   });
 });
