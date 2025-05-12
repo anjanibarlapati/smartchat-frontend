356628ced95f68978ef81a56402743b7740fa1d6
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-12 11:10:33 +0530

    Add theme reducer in redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { userReducer } from './reducer';
+import { userReducer } from './userReducer';
+import { themeReducer } from './themeReducer';
 
 export const store = configureStore({
     reducer: {
        user: userReducer,
+       theme: themeReducer,
     },
 });
+
+export type storeState = ReturnType<typeof store.getState>;
 
 
 
