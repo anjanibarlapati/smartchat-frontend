909d66066bbaa2347bea1a3dbf32b4f07f7d004a
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-12 11:09:02 +0530

    Add reducer for theme

---
+++ src/redux/themeReducer.ts
@@ -0,0 +1,22 @@
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ThemeState, ThemeMode } from '../types/Theme';
+
+
+const initialState: ThemeState = {
+  mode: 'system',
+};
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    setThemeMode: (state, action: PayloadAction<ThemeMode>) => {
+      state.mode = action.payload;
+    },
+  },
+});
+
+export const { setThemeMode } = themeSlice.actions;
+export const themeReducer = themeSlice.reducer;
+
