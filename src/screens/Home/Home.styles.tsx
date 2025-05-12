a50d1d1bdff87acfc2fe163e73992b722a048398
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-12 23:18:54 +0530

    Add styles for Home Screen

---
+++ src/screens/Home/Home.styles.tsx
@@ -0,0 +1,57 @@
+import {StyleSheet} from 'react-native';
+import {Theme} from '../../utils/themes';
+
+export const getStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.primaryBackground,
+    },
+    headerText: {
+        fontWeight: 'bold',
+         fontSize: 25,
+         fontFamily: 'Nunito',
+         color: theme.primaryColor,
+    },
+    bodyContainer: {
+      display: 'flex',
+      zIndex: -1,
+      alignItems: 'center',
+      position: 'relative',
+    },
+    totalContainer: {
+      flex: 1,
+    },
+    textContainer: {
+      marginTop: '75%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10,
+      margin: 10,
+    },
+    bodyText: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      textAlign: 'center',
+      fontFamily: 'Nunito',
+      color: theme.primaryTextColor,
+    },
+    addContactContainer: {
+      margin: 20,
+      display: 'flex',
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#008080',
+      height: '8%',
+      width: '14%',
+      borderRadius: 10,
+    },
+    addContactImage: {
+      height: 40,
+      width: 40,
+    },
+  });
