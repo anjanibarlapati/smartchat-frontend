c707a8107b23c15578e109e41687d11c5ec03b14
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-12 12:25:53 +0530

    Add a empty contact screen

---
+++ src/screens/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { ContactScreenNavigationProps } from '../../types/Navigations';
+// import { useNavigation } from '@react-navigation/native';
+
+export function Contact(): React.JSX.Element {
+//    const navigation = useNavigation<ContactScreenNavigationProps>();
+  return (
+      <>
+      </>
+  );
+}
