f388d107e53b7e38fa617764bb3e3d41db3b6663
varun-martha <varunkumar.martha@everest.engineering>
2025-05-12 18:37:39 +0530

    Add testcases for Profile screen ui

---
+++ src/screens/Profile/Profile.test.tsx
@@ -0,0 +1,95 @@
+import { Provider } from 'react-redux';
+import { render, screen, waitFor } from '@testing-library/react-native';
+import { Profile } from './Profile';
+import { store } from '../../redux/store';
+import { User } from '../../types/User';
+import { setUserDetails } from '../../redux/userReducer';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+jest.mock('../../utils/openCamera', () => ({
+  openCamera: jest.fn(),
+}));
+
+jest.mock('../../utils/openPhotoLibrary', () => ({
+  openPhotoLibrary: jest.fn(),
+}));
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: jest.fn(),
+  };
+});
+
+describe('Tests related to the Profile Screen', () => {
+    const user: User = {
+        firstName: 'Varun',
+        lastName: 'Kumar',
+        email: 'varun@gmail.com',
+        profilePicture: 'imagelink',
+        countryCode: '+91',
+        mobileNumber: '6303522765',
+    };
+    const navigation = { setOptions: jest.fn() };
+
+    const RenderProfileScreen = () => {
+        store.dispatch(setUserDetails(user));
+        return render(
+            <NavigationContainer>
+                <Provider store={store}>
+                    <Profile />
+                </Provider>
+            </NavigationContainer>
+        );
+    };
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
+    it('Should check all the elements', async() => {
+        (useNavigation as jest.Mock).mockReturnValue(navigation);
+        await waitFor(() => {
+            RenderProfileScreen();
+        });
+        expect(screen.getByLabelText('ProfilePicture')).toBeTruthy();
+        expect(screen.getByLabelText('editIcon')).toBeTruthy();
+        expect(screen.getByText('First Name')).toBeTruthy();
+        expect(screen.getByText('Last Name')).toBeTruthy();
+        expect(screen.getByText('Email')).toBeTruthy();
+        expect(screen.getByText('Contact')).toBeTruthy();
+        expect(screen.getByText('Change Password')).toBeTruthy();
+        expect(screen.getByText('Delete Account')).toBeTruthy();
+        expect(screen.getByText('Sign out')).toBeTruthy();
+    });
+
+    it('Should render all the user details', async() => {
+        (useNavigation as jest.Mock).mockReturnValue(navigation);
+        await waitFor(() => {
+            RenderProfileScreen();
+        });
+        expect(screen.getByText('Varun')).toBeTruthy();
+        expect(screen.getByText('Kumar')).toBeTruthy();
+        expect(screen.getByText('varun@gmail.com')).toBeTruthy();
+        expect(screen.getByText('+91 6303522765')).toBeTruthy();
+        expect(screen.getByLabelText('ProfilePicture').props.source).toEqual({uri: user.profilePicture});
+    });
+
+    it('Should set the navigation options correctly', async() => {
+        (useNavigation as jest.Mock).mockReturnValue(navigation);
+        await waitFor(() => {
+            RenderProfileScreen();
+        });
+        expect(navigation.setOptions).toHaveBeenCalledWith({
+            headerTitle: 'Profile',
+            headerTitleStyle: expect.any(Object),
+            headerStyle: expect.any(Object),
+        });
+    });
+
+});
