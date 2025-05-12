83e84af0515c7c0696cef449eebf10c6cad06bf3
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-12 23:16:36 +0530

    Implement Home Screen with start conversation text and add contact button

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-// import { HomeScreenNavigationProps } from '../../types/Navigations';
-// import { useNavigation } from '@react-navigation/native';
+import { Image, ScrollView, Text,  TouchableOpacity, View } from 'react-native';
+import { getStyles } from './Home.styles';
+import { useAppTheme } from '../../hooks/appTheme';
+import { Theme } from '../../utils/themes';
+import { useNavigation } from '@react-navigation/native';
+import { ContactScreenNavigationProps } from '../../types/Navigations';
 
 export function Home(): React.JSX.Element {
-    // const navigation = useNavigation<HomeScreenNavigationProps>();
-
+    const theme : Theme = useAppTheme();
+    const styles = getStyles(theme);
+    const navigation = useNavigation<ContactScreenNavigationProps >();
   return (
-      <View>
-        <Text>Welcome to Home</Text>
+      <View style={styles.container}>
+        <View style={styles.totalContainer}>
+          <ScrollView contentContainerStyle={styles.bodyContainer}>
+            <View style={styles.textContainer}>
+            <Text style={styles.bodyText}>Start Conversations with your closed ones</Text>
+            </View>
+          </ScrollView>
+            <TouchableOpacity onPress={() => navigation.navigate('Contact')} style={styles.addContactContainer}>
+              <Image source= {require('../../../assets/icons/contacts-add-icon.png')} style={styles.addContactImage} accessibilityLabel="addContact"/>
+            </TouchableOpacity>
+        </View>
       </View>
   );
 }
+
