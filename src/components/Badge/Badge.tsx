3fc875f5dbf2aa298e3ab622c640d0b399756377
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-13 15:33:29 +0530

    Implement the bage component

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Badgestyles} from './Badge.styles';
+import {Theme} from '../../utils/themes';
+import {useAppTheme} from '../../hooks/appTheme';
+
+type BadgeProps = {
+  value: string;
+};
+
+export function Badge({value}: BadgeProps) {
+  const theme: Theme = useAppTheme();
+  const styles = Badgestyles(theme);
+  return (
+    <View style={styles.container}>
+      {value ? <Text style={styles.text}>{value}</Text> : null}
+    </View>
+  );
+}
