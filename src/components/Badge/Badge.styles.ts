4e13b0368b6032c2ff0959edeb8cf9e96120cf30
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-13 15:34:00 +0530

    Add styles for Badge

---
+++ src/components/Badge/Badge.styles.ts
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+import {Theme} from '../../utils/themes';
+
+export const Badgestyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      position: 'absolute',
+      top: 10,
+      right: 10,
+      minWidth: 24,
+      height: 24,
+      paddingHorizontal: 6,
+      borderRadius: 12,
+      backgroundColor: theme.badgeBackground,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      color: theme.badgeTextColor,
+      fontSize: 12,
+      fontWeight: 'bold',
+    },
+  });
