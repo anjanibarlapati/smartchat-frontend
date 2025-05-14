9615d79b8d1dc181a76dbd1ed92c5d8ba5ecf343
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-14 10:43:18 +0530

    Remove the extra spaces

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
-import { Image, ScrollView, Text,  TouchableOpacity, View } from 'react-native';
-import { getStyles } from './Home.styles';
-import { useAppTheme } from '../../hooks/appTheme';
-import { Theme } from '../../utils/themes';
-import { useNavigation } from '@react-navigation/native';
-import { ContactScreenNavigationProps } from '../../types/Navigations';
-
+import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import {getStyles} from './Home.styles';
+import {useAppTheme} from '../../hooks/appTheme';
+import {Theme} from '../../utils/themes';
+import {useNavigation} from '@react-navigation/native';
+import {ContactScreenNavigationProps} from '../../types/Navigations';
 
 export function Home(): React.JSX.Element {
-    const theme : Theme = useAppTheme();
-    const styles = getStyles(theme);
-    const navigation = useNavigation<ContactScreenNavigationProps >();
+  const theme: Theme = useAppTheme();
+  const styles = getStyles(theme);
+  const navigation = useNavigation<ContactScreenNavigationProps>();
   return (
-      <View style={styles.container}>
- 
-        <View style={styles.totalContainer}>
-          <ScrollView contentContainerStyle={styles.bodyContainer}>
-            <View style={styles.textContainer}>
-            <Text style={styles.bodyText}>Start Conversations with your closed ones</Text>
-            </View>
-          </ScrollView>
-            <TouchableOpacity onPress={() => navigation.navigate('Contact')} style={styles.addContactContainer}>
-              <Image source= {require('../../../assets/icons/contacts-add-icon.png')} style={styles.addContactImage} accessibilityLabel="addContact"/>
-            </TouchableOpacity>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.totalContainer}>
+        <ScrollView contentContainerStyle={styles.bodyContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.bodyText}>
+              Start Conversations with your closed ones
+            </Text>
+          </View>
+        </ScrollView>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Contact')}
+          style={styles.addContactContainer}>
+          <Image
+            source={require('../../../assets/icons/contacts-add-icon.png')}
+            style={styles.addContactImage}
+            accessibilityLabel="addContact"
+          />
+        </TouchableOpacity>
       </View>
+    </View>
   );
 }
-
