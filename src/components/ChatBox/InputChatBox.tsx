4da85e89b6008f4589784881f31a5ded2157b010
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-14 11:16:20 +0530

    Adjust the prettier

--- src/components/ChatBox/InputChatBox.tsx
+++ src/components/ChatBox/InputChatBox.tsx
@@ -1,21 +1,19 @@
 import React, {useState} from 'react';
 import {TextInput, View, KeyboardAvoidingView} from 'react-native';
-import { ChatInputStyles } from './InputChatBox.styles';
+import {ChatInputStyles} from './InputChatBox.styles';
 import {Theme} from '../../utils/themes';
 import {useAppTheme} from '../../hooks/appTheme';
 // type Props = {
 //   onSend: (message: string) => void;
 // };
 
-
 export function InputChatBox() {
-const theme:Theme = useAppTheme();
-const styles = ChatInputStyles(theme);
+  const theme: Theme = useAppTheme();
+  const styles = ChatInputStyles(theme);
   const [message, setMessage] = useState('');
- 
 
   return (
-   <KeyboardAvoidingView
+    <KeyboardAvoidingView
     //   behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     //  keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}
     >
