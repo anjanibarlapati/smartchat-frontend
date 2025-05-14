eef9a8f50ccaf61b077beffc36f5d360c7d6bdfa
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-14 11:37:54 +0530

    Change the order of import statements

--- src/components/ChatBox/InputChatBox.test.tsx
+++ src/components/ChatBox/InputChatBox.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import {render, screen} from '@testing-library/react-native';
 import {InputChatBox} from './InputChatBox';
-import {Provider} from 'react-redux';
 import {store} from '../../redux/store';
 
 describe('InputChatBox', () => {
