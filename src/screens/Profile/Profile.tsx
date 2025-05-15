8ad9a5532b3212513fb2135121c479ead92833c6
varun-martha <varunkumar.martha@everest.engineering>
2025-05-15 15:01:27 +0530

    Implement deleteAccount, signout, handleRemoveProfile and uploadNewProfilePicture methods

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,19 +1,35 @@
-import { useEffect } from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+import { Alert, Image, Text, TouchableOpacity, View } from 'react-native';
+import EncryptedStorage from 'react-native-encrypted-storage';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
 import { ProfileInfoTile } from '../../components/ProfileInfoTile/ProfileInfoTile';
+import { ProfilePicturePickerModal } from '../../components/ProfilePicturePickerModal/ProfilePicturePickerModal';
 import { useAppTheme } from '../../hooks/appTheme';
+import LoadingScreen from '../Loading/Loading';
+import { deleteAccount, removeProfilePic, updateProfilePic } from './Profile.handler';
 import { getStyles } from './Profile.styles';
-import { userState } from '../../types/User';
+import { storeState } from '../../redux/store';
+import { setUserProperty } from '../../redux/userReducer';
+import { WelcomeScreenNavigationProps } from '../../types/Navigations';
+import { UploadImage } from '../../types/UploadImage';
+import { getTokens } from '../../utils/getTokens';
 import { Theme } from '../../utils/themes';
 
 export const Profile = (): React.JSX.Element => {
 
-  const userDetails = useSelector((state: userState) => state.user);
+  const userDetails = useSelector((state: storeState) => state.user);
   const theme: Theme = useAppTheme();
   const styles = getStyles(theme);
-  const navigation = useNavigation();
+  const navigation = useNavigation<WelcomeScreenNavigationProps>();
+  const dispatch = useDispatch();
+
+  const [editField, setEditField] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const [profilePicUrl, setProfilePicUrl] = useState<string | null>(userDetails.profilePicture || null);
+  const [uploadImage, setUploadImage] = useState<UploadImage | string | null>(null);
+  const [visibleProfilePicModal, setVisibleProfilePicModal] = useState(false);
+  const imageUploaded = useRef(false);
 
   useEffect(() => {
         navigation.setOptions({
@@ -23,13 +39,111 @@ export const Profile = (): React.JSX.Element => {
         });
   }, [navigation, styles.headerBackgroundColor, styles.headerTitleStyle]);
 
+  useEffect(() => {
+      const handleUploadProfilePic = async () => {
+        if(!uploadImage || imageUploaded.current) {
+          return;
+        }
+        imageUploaded.current = true;
+        setLoading(true);
+        try {
+          const tokens = await getTokens(userDetails.mobileNumber);
+          if(!tokens) {
+            Alert.alert('Invalid Access tokens');
+            return;
+          }
+          const formData = new FormData();
+          formData.append('profilePicture', uploadImage);
+          formData.append('mobileNumber', userDetails.mobileNumber);
+          const response = await updateProfilePic(formData, tokens.access_token);
+          const result = await response.json();
+          if (response.ok) {
+            const updatedUser = {
+              ...userDetails,
+              profilePicture: result.user.profilePicture,
+            };
+            dispatch(setUserProperty({
+              property: 'profilePicture',
+              value: result.user.profilePicture,
+            }));
+            setProfilePicUrl(result.user.profilePicture);
+            await EncryptedStorage.setItem('User Data', JSON.stringify(updatedUser));
+            Alert.alert('Profile picture updated');
+          } else {
+            Alert.alert(result.message || 'Failed to upload profile picture');
+          }
+        } catch (error) {
+          Alert.alert('Upload failed', 'Please try again later!');
+        } finally {
+          setUploadImage(null);
+          setLoading(false);
+        }
+      };
+
+      if(uploadImage) {
+        handleUploadProfilePic();
+      }
+
+  }, [dispatch, uploadImage, userDetails]);
+
+  const signout = async() => {
+    await EncryptedStorage.clear();
+    navigation.navigate('WelcomeScreen');
+  };
+
+  const handleRemoveProfile = async() => {
+    const tokens = await getTokens(userDetails.mobileNumber);
+    if(!tokens) {
+      Alert.alert('Invalid Access tokens');
+      return;
+    }
+    const response = await removeProfilePic(profilePicUrl as string, userDetails.mobileNumber, tokens.access_token);
+    if(response.ok) {
+        const updatedUser = {
+            ...userDetails,
+            profilePicture: '',
+        };
+        dispatch(setUserProperty({
+            property: 'profilePicture',
+            value: '',
+        }));
+        await EncryptedStorage.setItem('User Data', JSON.stringify(updatedUser));
+        Alert.alert('Successfully Removed');
+    }
+    setProfilePicUrl('');
+    setVisibleProfilePicModal(false);
+  };
+
+  const accountDelete = async() => {
+    const tokens = await getTokens(userDetails.mobileNumber);
+    if(!tokens) {
+      Alert.alert('Invalid Access tokens');
+      return;
+    }
+    try{
+      const response = await deleteAccount(userDetails.mobileNumber, tokens.access_token);
+      if(response.ok) {
+        await EncryptedStorage.clear();
+        navigation.navigate('WelcomeScreen');
+      } else {
+        Alert.alert('Failed to delete');
+      }
+    } catch(error) {
+      Alert.alert('Failed to delete!');
+    }
+  };
+
+  if(isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profileImg}>
         <Image
           source={
-            userDetails.profilePicture
-              ? {uri: userDetails.profilePicture}
+            profilePicUrl?.trim()
+              ? {uri: profilePicUrl}
               : require('../../../assets/images/profileImage.png')
           }
           resizeMode="cover"
@@ -39,6 +153,7 @@ export const Profile = (): React.JSX.Element => {
         <TouchableOpacity
           style={styles.editProfileImgBox}
           onPress={() => {
+            setVisibleProfilePicModal(true);
           }}>
           <Image
             source={require('../../../assets/icons/camera-icon.png')}
@@ -52,28 +167,38 @@ export const Profile = (): React.JSX.Element => {
           label={'First Name'}
           value={userDetails.firstName}
           image={require('../../../assets/icons/user-icon.png')}
+          editField={editField}
+          setEditField={setEditField}
         />
         <ProfileInfoTile
           label={'Last Name'}
           value={userDetails.lastName}
           image={require('../../../assets/icons/user-icon.png')}
+          editField={editField}
+          setEditField={setEditField}
         />
         <ProfileInfoTile
           label={'Email'}
           value={userDetails.email}
           image={require('../../../assets/icons/email.png')}
+          editField={editField}
+          setEditField={setEditField}
         />
         <ProfileInfoTile
           label={'Contact'}
           value={`${userDetails.countryCode} ${userDetails.mobileNumber}`}
           image={require('../../../assets/icons/contact.png')}
+          editField={editField}
+          setEditField={setEditField}
         />
         <ProfileInfoTile
           label={'Change Password'}
           value={'*******'}
           image={require('../../../assets/icons/password.png')}
+          editField={editField}
+          setEditField={setEditField}
         />
-        <TouchableOpacity style={styles.box}>
+        <TouchableOpacity style={styles.box} onPress={() => {accountDelete();}}>
           <Image
             source={require('../../../assets/icons/delete.png')}
             resizeMode="contain"
@@ -81,7 +206,7 @@ export const Profile = (): React.JSX.Element => {
           />
           <Text style={styles.deleteText}>Delete Account</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.box}>
+        <TouchableOpacity style={styles.box} onPress={signout}>
           <Image
             source={require('../../../assets/icons/logout.png')}
             resizeMode="contain"
@@ -89,6 +214,13 @@ export const Profile = (): React.JSX.Element => {
           />
           <Text style={styles.valueText}>Sign out</Text>
         </TouchableOpacity>
+        <ProfilePicturePickerModal
+          isEditingProfilePicture={visibleProfilePicModal}
+          close={() => {setVisibleProfilePicModal(false);}}
+          profilePicture={userDetails.profilePicture || null}
+          setProfilePic={setUploadImage}
+          remove={handleRemoveProfile}
+        />
       </View>
     </View>
   );
