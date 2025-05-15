94980b52852459e5c716a71976c5f95f8aaf9fb6
Shailaja Nimmagari <admin@ShailajaCurious-Intern.local>
2025-05-15 10:28:04 +0530

    Implement unread screen with initial message along with styling

---
+++ src/screens/Unread/Unread.tsx
@@ -0,0 +1,41 @@
+import { getStyles } from '../Home/Home.styles';
+import {useAppTheme} from '../../hooks/appTheme';
+import React from 'react';
+import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {AllChatsTabScreenNavigationProps, ContactScreenNavigationProps} from '../../types/Navigations';
+import {Theme} from '../../utils/themes';
+import {getStyles as UnreadStyles} from './Unread.styles';
+
+export function Unread(): React.JSX.Element {
+  const theme: Theme = useAppTheme();
+  const styles = getStyles(theme);
+  const Ustyles = UnreadStyles(theme);
+  const navigation = useNavigation<ContactScreenNavigationProps>();
+  const navigationToAllChats = useNavigation<AllChatsTabScreenNavigationProps>();
+  return (
+    <View style={styles.container}>
+      <View style={styles.totalContainer}>
+        <ScrollView contentContainerStyle={styles.bodyContainer}>
+          <View style={styles.textContainer}>
+            <Text style={Ustyles.bodyText}>
+              No unread chats here
+            </Text>
+            <TouchableOpacity onPress={() => navigationToAllChats.navigate('AllChatsTab')}>
+            <Text style={Ustyles.viewText}>View all chats</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Contact')}
+          style={styles.addContactContainer}>
+          <Image
+            source={require('../../../assets/icons/contacts-add-icon.png')}
+            style={styles.addContactImage}
+            accessibilityLabel="addContact"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
