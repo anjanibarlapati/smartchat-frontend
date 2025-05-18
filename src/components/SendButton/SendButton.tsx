262ee3647654420f3fe10b916ffdd4bcc9f0a8eb
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-18 21:47:14 +0530

    Add send icon source based on theme

--- src/components/SendButton/SendButton.tsx
+++ src/components/SendButton/SendButton.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {View, Image, TouchableOpacity} from 'react-native';
 import {styles} from './SendButton.styles';
+import { Theme } from '../../utils/themes';
+import { useAppTheme } from '../../hooks/appTheme';
 
 export function SendButton() {
+  const theme: Theme = useAppTheme();
   return (
     <View>
       <TouchableOpacity>
         <Image
-          source={require('../../../assets/icons/send.png')}
+          source={theme.images.send}
           style={styles.sendButtonIcon}
+          accessibilityLabel="send-icon"
         />
       </TouchableOpacity>
     </View>
