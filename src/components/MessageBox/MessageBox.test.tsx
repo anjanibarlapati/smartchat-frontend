43f779b6094c0e435e6c41fd0305e468359b8d67
MamathaNiyal23 <mamatha.niyal@everest.engineering>
2025-05-18 18:37:47 +0530

    Modify test file by wrapping the component in a provider

--- src/components/MessageBox/MessageBox.test.tsx
+++ src/components/MessageBox/MessageBox.test.tsx
@@ -1,79 +1,89 @@
 import { render, screen } from '@testing-library/react-native';
 import { MessageBox } from './MessageBox';
+import { Provider } from 'react-redux';
+import { store } from '../../redux/store';
+import { MessageProps } from '../../types/MessageProps';
 
 
 jest.mock('../../../assets/images/singleTick.png', () => 'singleTick');
 jest.mock('../../../assets/images/doubleTick.png', () => 'doubleTick');
 jest.mock('../../../assets/images/readTick.png', () => 'readTick');
 
-describe('MessageBox Component Check', () => {
 
-  it('Should render message and timestamp', () => {
-    render(
+const renderMessageBox = ({
+  message = '',
+  timestamp = '',
+  status = 'sent',
+  isSender = false,
+}: Partial<MessageProps> = {}) => {
+  return render(
+    <Provider store={store}>
       <MessageBox
-        message="Hello, Mamatha!"
-        timestamp="12:45 PM"
-        status="sent"
-        isSender={true}
+        message={message}
+        timestamp={timestamp}
+        status={status}
+        isSender={isSender}
       />
-    );
+    </Provider>
+  );
+};
+
+describe('MessageBox Component Check', () => {
+  it('Should render message and timestamp', () => {
+    renderMessageBox({
+      message: 'Hello, Mamatha!',
+      timestamp: '12:45 PM',
+      status: 'sent',
+      isSender: true,
+    });
 
     expect(screen.getByText('Hello, Mamatha!')).toBeTruthy();
     expect(screen.getByText('12:45 PM')).toBeTruthy();
   });
 
-  it(' Should not render tick icon if isSender is false', () => {
-    render(
-      <MessageBox
-        message="Hi!"
-        timestamp="1:00 PM"
-        status="read"
-        isSender={false}
-      />
-    );
+  it('Should not render tick icon if isSender is false', () => {
+    renderMessageBox({
+      message: 'Hi!',
+      timestamp: '1:00 PM',
+      status: 'read',
+      isSender: false,
+    });
 
-    expect(screen.queryByAccessibilityHint('sent-tick-icon')).toBeNull();
-    expect(screen.queryByAccessibilityHint('delivered-tick-icon')).toBeNull();
-    expect(screen.queryByAccessibilityHint('read-tick-icon')).toBeNull();
+    expect(screen.queryByLabelText('sent-tick-icon')).toBeNull();
+    expect(screen.queryByLabelText('delivered-tick-icon')).toBeNull();
+    expect(screen.queryByLabelText('read-tick-icon')).toBeNull();
   });
 
   it('Should render single tick icon when status is "sent"', () => {
-    render(
-      <MessageBox
-        message="Test Sent"
-        timestamp="1:01 PM"
-        status="sent"
-        isSender={true}
-      />
-    );
+    renderMessageBox({
+      message: 'Test Sent',
+      timestamp: '1:01 PM',
+      status: 'sent',
+      isSender: true,
+    });
 
-    expect(screen.getByAccessibilityHint('sent-tick-icon')).toBeTruthy();
+    expect(screen.getByLabelText('sent-tick-icon')).toBeTruthy();
   });
 
   it('Should render double tick icon when status is "delivered"', () => {
-    render(
-      <MessageBox
-        message="Test Delivered"
-        timestamp="1:02 PM"
-        status="delivered"
-        isSender={true}
-      />
-    );
+    renderMessageBox({
+      message: 'Test Delivered',
+      timestamp: '1:02 PM',
+      status: 'delivered',
+      isSender: true,
+    });
 
-    expect(screen.getByAccessibilityHint('delivered-tick-icon')).toBeTruthy();
+    expect(screen.getByLabelText('delivered-tick-icon')).toBeTruthy();
   });
 
   it('Should render read tick icon when status is "read"', () => {
-    render(
-      <MessageBox
-        message="Test Read"
-        timestamp="1:03 PM"
-        status="read"
-        isSender={true}
-      />
-    );
+    renderMessageBox({
+      message: 'Test Read',
+      timestamp: '1:03 PM',
+      status: 'read',
+      isSender: true,
+    });
 
-    expect(screen.getByAccessibilityHint('read-tick-icon')).toBeTruthy();
+    expect(screen.getByLabelText('read-tick-icon')).toBeTruthy();
   });
-
 });
