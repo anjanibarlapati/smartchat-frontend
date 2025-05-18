b7b8ddd69abbc86acd22bb27d6b2de3206e59e71
Shailaja Nimmagari <admin@ShailajaCurious-Intern.local>
2025-05-18 18:06:08 +0530

    Add Chat options modal with styling

---
+++ src/components/ChatOptionsModal/ChatOptionsModal.styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+import { Theme } from '../../utils/themes';
+
+export const getStyles = (theme:Theme)=> StyleSheet.create({
+    overlay: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+    },
+    modalContainer: {
+      position: 'absolute',
+      width: 192,
+      height: 68,
+      marginLeft: 201,
+      marginTop: 113,
+      backgroundColor: theme.primaryBackground,
+      shadowColor: theme.primaryShadowColor,
+      shadowOpacity: 0.18,
+      shadowRadius: 5,
+      borderRadius: 10,
+      paddingHorizontal: 19,
+      paddingVertical: 10,
+    },
+    clearChatButton: {
+        height: 26,
+        width: 173,
+        justifyContent: 'center',
+      },
+      blockButton: {
+        height: 15,
+        width: 88,
+        marginTop: 5,
+        justifyContent: 'center',
+      },
+    text: {
+      fontFamily: 'Nunito',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: 14,
+      lineHeight:16,
+      textAlign: 'left',
+      color: theme.primaryTextColor,
+    },
+  });
