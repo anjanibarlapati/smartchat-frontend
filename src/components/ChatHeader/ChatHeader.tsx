d5daffdf46d1b3edbc047909bd927856f205032d
varun-martha <varunkumar.martha@everest.engineering>
2025-05-18 23:53:09 +0530

    Implement ChatHeader component and add styles to it

---
+++ src/components/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,35 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { getStyles } from './ChatHeader.styles';
+import { useAppTheme } from '../../hooks/appTheme';
+import { Theme } from '../../utils/themes';
+
+interface ChatHeaderProps {
+    name: string,
+    mobileNumber: string,
+    profilePic?: string | null
+}
+
+export const ChatHeader = ({name, mobileNumber, profilePic}: ChatHeaderProps) => {
+      const theme: Theme = useAppTheme();
+      const styles = getStyles(theme);
+
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity style={styles.backImage}>
+            <Image source={require('../../../assets/images/chevron.png')} style={styles.backIcon} accessibilityLabel="Back-Icon"/>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profielDetailsContainer}>
+            <Image source={profilePic ? {uri: profilePic} : require('../../../assets/images/profileImage.png')} resizeMode="contain" style={styles.profileImage} accessibilityLabel="Profile-Image"/>
+            <View style={styles.profileDetails}>
+                <Text
+                    numberOfLines={1}
+                    ellipsizeMode="tail"
+                    style={styles.nameText}>
+                    {name}
+                </Text>
+                <Text style={styles.numberText}>{mobileNumber}</Text>
+            </View>
+        </TouchableOpacity>
+    </View>
+  );
+};
