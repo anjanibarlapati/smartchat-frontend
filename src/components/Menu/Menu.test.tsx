a6247d8c9b400b640180d619cea915aafe80a705
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-05-19 23:30:01 +0530

    Update tests for menu component

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,10 +1,57 @@
-import { render, screen } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import { Menu } from './Menu';
+import { Provider } from 'react-redux';
+import { store } from '../../redux/store';
 
 describe('Tests related to Menu component', () => {
+  it('should render the menu image', () => {
+    render(
+      <Provider store={store}>
+        <Menu />
+      </Provider>
+    );
+    expect(screen.getByLabelText('Menu-Image')).toBeTruthy();
+  });
 
-    it('Should render the menu image', () => {
-        render(<Menu />);
-        expect(screen.getByLabelText('Menu-Image')).toBeTruthy();
-    });
+  it('should close the modal when "Clear Chat" is pressed', () => {
+    render(
+      <Provider store={store}>
+        <Menu />
+      </Provider>
+    );
+
+    fireEvent.press(screen.getByLabelText('Menu-Image'));
+    expect(screen.getByText('Clear Chat')).toBeTruthy();
+
+    fireEvent.press(screen.getByText('Clear Chat'));
+    expect(screen.queryByText('Clear Chat')).toBeNull();
+  });
+
+  it('should close the modal when "Block" is pressed', () => {
+    render(
+      <Provider store={store}>
+        <Menu />
+      </Provider>
+    );
+
+    fireEvent.press(screen.getByLabelText('Menu-Image'));
+    expect(screen.getByText('Block')).toBeTruthy();
+
+    fireEvent.press(screen.getByText('Block'));
+    expect(screen.queryByText('Block')).toBeNull();
+  });
+
+  it('should close the modal when clicking outside the modal (overlay)', () => {
+    render(
+      <Provider store={store}>
+        <Menu />
+      </Provider>
+    );
+
+    fireEvent.press(screen.getByLabelText('Menu-Image'));
+    expect(screen.getByText('Clear Chat')).toBeTruthy();
+
+    fireEvent.press(screen.getByLabelText('overlay'));
+    expect(screen.queryByText('Clear Chat')).toBeNull();
+  });
 });
