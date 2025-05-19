f956fb0fcb81c3db36a8d717f659bc11af5900d4
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-05-19 23:22:58 +0530

    Add go back navigation from screen

--- src/components/ChatHeader/ChatHeader.tsx
+++ src/components/ChatHeader/ChatHeader.tsx
@@ -2,6 +2,7 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { getStyles } from './ChatHeader.styles';
 import { useAppTheme } from '../../hooks/appTheme';
 import { Theme } from '../../utils/themes';
+import { useNavigation } from '@react-navigation/native';
 
 interface ChatHeaderProps {
     name: string,
@@ -12,10 +13,14 @@ interface ChatHeaderProps {
 export const ChatHeader = ({name, mobileNumber, profilePic}: ChatHeaderProps) => {
       const theme: Theme = useAppTheme();
       const styles = getStyles(theme);
+      const navigation = useNavigation();
+      const handleNavigation = ()=>{
+         navigation.goBack();
+      };
 
   return (
     <View style={styles.container}>
-        <TouchableOpacity style={styles.backImage}>
+        <TouchableOpacity style={styles.backImage} onPress={handleNavigation}>
             <Image source={require('../../../assets/images/chevron.png')} style={styles.backIcon} accessibilityLabel="Back-Icon"/>
         </TouchableOpacity>
         <TouchableOpacity style={styles.profielDetailsContainer}>
