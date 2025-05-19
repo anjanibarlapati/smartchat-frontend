f7b1d84fd6256d261e65af36010df5bf886c1e12
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-05-19 23:29:36 +0530

    Update menu component with chat options modal

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,33 @@
-import { Image, TouchableOpacity } from 'react-native';
-import { styles } from './Menu.styles';
+import {Image, TouchableOpacity, View} from 'react-native';
+import {styles} from './Menu.styles';
+import {ChatOptionsModal} from '../ChatOptionsModal/ChatOptionsModal';
+import {useState} from 'react';
 
 export const Menu = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const handleClearChat = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleBlock = () => {
+    setIsModalVisible(false);
+  };
   return (
-    <TouchableOpacity>
-        <Image source={require('../../../assets/icons/menu.png')} style={styles.menuIcon} accessibilityLabel="Menu-Image"/>
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+        <Image
+          source={require('../../../assets/icons/menu.png')}
+          style={styles.menuIcon}
+          accessibilityLabel="Menu-Image"
+        />
+      </TouchableOpacity>
+      <ChatOptionsModal
+        visible={isModalVisible}
+        onClearChat={handleClearChat}
+        onBlock={handleBlock}
+        onClose={() => setIsModalVisible(false)}
+      />
+    </View>
   );
 };
