805dc011e7eaf2d15303dcbd9b2400745a699ec3
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-19 12:42:20 +0530

    Modify stylings for contact card component

--- src/components/ContactCard/ContactCard.styles.ts
+++ src/components/ContactCard/ContactCard.styles.ts
@@ -2,22 +2,15 @@ import { StyleSheet } from 'react-native';
 import { Theme } from '../../utils/themes';
 
 const getStyles = (theme: Theme) => StyleSheet.create({
-    container: {
-      display:'flex',
-      flexDirection:'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      backgroundColor: theme.primaryBackground,
-      paddingVertical:10,
-      paddingLeft:15,
-      paddingRight: 40,
-    },
     body:{
         display:'flex',
         flexDirection:'row',
-        justifyContent:'flex-start',
         alignItems:'center',
         gap:20,
+        backgroundColor: theme.primaryBackground,
+        paddingVertical:10,
+        paddingLeft:15,
+        paddingRight: 40,
     },
     profileIcon: {
         width: 55,
@@ -28,11 +21,19 @@ const getStyles = (theme: Theme) => StyleSheet.create({
         display: 'flex',
         flexDirection:'column',
         gap:5,
+        width:'80%',
+    },
+    nameInviteContainer:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        gap:10,
     },
     contactName: {
         color: theme.primaryTextColor,
         fontSize: 18,
-        fontFamily:'Nunito',
+        width:'75%',
+        overflow:'hidden',
     },
     contactNumber:{
         color: theme.secondaryTextColor,
--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -1,33 +1,36 @@
 import { Image, Pressable, Text, View } from 'react-native';
+import { getStyles } from './ContactCard.styles';
 import { useAppTheme } from '../../hooks/appTheme';
-import { Theme } from '../../utils/themes';
-import {getStyles} from './ContactCard.styles';
 import { Contact } from '../../types/Contacts';
 import { sendSmsInvite } from '../../utils/sendSmsInvite';
+import { Theme } from '../../utils/themes';
+
 
 function ContactCard ({contact}: {contact: Contact}):React.JSX.Element {
 
     const theme: Theme = useAppTheme();
     const styles = getStyles(theme);
     return (
-        <View style={styles.container}>
+        // <View style={styles.container}>
             <View style={styles.body}>
-                <Image
-                    style={styles.profileIcon}
-                    source={contact.profilePicture ? {uri: contact.profilePicture}
-                    : require('../../../assets/images/profileImage.png') }
-                    accessibilityLabel="profile-image"
-                    resizeMode="cover"
-                />
+                    <Image
+                        style={styles.profileIcon}
+                        source={contact.profilePicture ? {uri: contact.profilePicture}
+                        : require('../../../assets/images/profileImage.png') }
+                        accessibilityLabel="profile-image"
+                        resizeMode="cover"
+                    />
                 <View style={styles.contactContainer}>
-                    <Text style={styles.contactName}>{contact.name}</Text>
+                    <View style={styles.nameInviteContainer}>
+                        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.contactName}>{contact.name}</Text>
+                        { !contact.doesHaveAccount && <Pressable onPress= {()=> sendSmsInvite(contact.mobileNumber)}>
+                            <Text style={styles.inviteText}>Invite</Text>
+                        </Pressable> }
+                    </View>
                     <Text style={styles.contactNumber}>{contact.mobileNumber}</Text>
                 </View>
             </View>
-            { !contact.doesHaveAccount && <Pressable onPress= {()=> sendSmsInvite(contact.mobileNumber)}>
-                <Text style={styles.inviteText}>Invite</Text>
-            </Pressable> }
-        </View>
+        // </View>
     );
 }
 
