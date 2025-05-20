7c8e046cb2bb039e15f688ccfc3c7352bf6d7598
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-20 13:08:59 +0530

    Update alert message in catch block

--- src/permissions/permissions.ts
+++ src/permissions/permissions.ts
@@ -1,58 +1,54 @@
-import { Alert, Platform } from 'react-native';
-import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions';
+import {Alert, Platform} from 'react-native';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 
-const requestPermission = async(permissionType: string) => {
-    try{
-        let permission;
-        switch(permissionType) {
-            case 'camera':
-                permission = Platform.select({
-                    ios: PERMISSIONS.IOS.CAMERA,
-                    android: PERMISSIONS.ANDROID.CAMERA,
-                });
-                break;
-            case 'media':
-                permission = Platform.select({
-                    ios: PERMISSIONS.IOS.PHOTO_LIBRARY,
-                    android: PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
-                });
-                break;
-            case 'contacts':
-                permission = Platform.select({
-                    ios: PERMISSIONS.IOS.CONTACTS,
-                    android: PERMISSIONS.ANDROID.READ_CONTACTS,
-                });
-                break;
-            case 'send-sms':
-                permission = Platform.select({
-                    ios: undefined,
-                    android: PERMISSIONS.ANDROID.SEND_SMS,
-                });
-                break;
-
-        }
-        if(!permission){
-            return true;
-        }
-        if(permission){
-            const result = await check(permission);
-            if(result === RESULTS.DENIED) {
-                const grantResult = await request(permission);
-                if(grantResult === RESULTS.GRANTED) {
-                    return true;
-                }
-            }
-            else if(result === RESULTS.GRANTED) {
-                return true;
-            }
-            return false;
-        }
-        return false;
+const requestPermission = async (permissionType: string) => {
+  try {
+    let permission;
+    switch (permissionType) {
+      case 'camera':
+        permission = Platform.select({
+          ios: PERMISSIONS.IOS.CAMERA,
+          android: PERMISSIONS.ANDROID.CAMERA,
+        });
+        break;
+      case 'media':
+        permission = Platform.select({
+          ios: PERMISSIONS.IOS.PHOTO_LIBRARY,
+          android: PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
+        });
+        break;
+      case 'contacts':
+        permission = Platform.select({
+          ios: PERMISSIONS.IOS.CONTACTS,
+          android: PERMISSIONS.ANDROID.READ_CONTACTS,
+        });
+        break;
+      case 'send-sms':
+        permission = Platform.select({
+          ios: undefined,
+          android: PERMISSIONS.ANDROID.SEND_SMS,
+        });
+        break;
     }
-    catch(error) {
-        Alert.alert('error occurred');
+    if (!permission) {
+      return true;
     }
+    if (permission) {
+      const result = await check(permission);
+      if (result === RESULTS.DENIED) {
+        const grantResult = await request(permission);
+        if (grantResult === RESULTS.GRANTED) {
+          return true;
+        }
+      } else if (result === RESULTS.GRANTED) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  } catch (error) {
+    Alert.alert('Something went wrong!');
+  }
 };
 
-
-export { requestPermission };
+export {requestPermission};
