119f80e9c3e8a71e2e2737c6766e9bf8505a9219
Anjali Gogu <anjali.gogu@everest.engineering>
2025-05-20 23:21:39 +0530

    Add the clear storage and keychain data function on first launch in iOS

--- ios/SmartChat/AppDelegate.swift
+++ ios/SmartChat/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Security 
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    
+    if !UserDefaults.standard.bool(forKey: "hasRunBefore") {
+      clearApplicationSupportDirectory()
+      clearKeychain()
+      UserDefaults.standard.set(true, forKey: "hasRunBefore")
+      UserDefaults.standard.synchronize()
+    }
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -30,6 +39,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     )
 
     return true
+  }
+
+  func clearApplicationSupportDirectory() {
+    let fileManager = FileManager.default
+
+    do {
+      let appSupportURL = try fileManager.url(
+        for: .applicationSupportDirectory,
+        in: .userDomainMask,
+        appropriateFor: nil,
+        create: true
+      )
+
+      let contents = try fileManager.contentsOfDirectory(atPath: appSupportURL.path)
+
+      for item in contents {
+        let itemPath = appSupportURL.appendingPathComponent(item).path
+        try fileManager.removeItem(atPath: itemPath)
+      }
+    } catch {
+      print("Error clearing Application Support directory: \(error)")
+    }
+  }
+
+  func clearKeychain() {
+    let secItemClasses = [
+      kSecClassGenericPassword,
+      kSecClassInternetPassword,
+      kSecClassCertificate,
+      kSecClassKey,
+      kSecClassIdentity
+    ]
+
+    for itemClass in secItemClasses {
+      let query: [String: Any] = [kSecClass as String: itemClass]
+      SecItemDelete(query as CFDictionary)
+    }
   }
 }
 
