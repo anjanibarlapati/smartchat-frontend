7bd14b7e96254136e4564ee04a26a01585df3e77
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-21 01:35:26 +0530

    Update get contacts function tests fot handle parsed mobile numbers

--- src/utils/getContactDetails.test.ts
+++ src/utils/getContactDetails.test.ts
@@ -1,47 +1,63 @@
-import {getContactsDetails} from './getContactsDetails';
-import {Contact as ContactType} from '../types/Contacts';
-import {Contact} from 'react-native-contacts';
-import {fetchContacts} from '../screens/Contact/Contact.service';
+import { getContactsDetails } from './getContactsDetails';
+import { Contact as ContactType } from '../types/Contacts';
+import { Contact } from 'react-native-contacts';
+import { fetchContacts } from '../screens/Contact/Contact.service';
 
 jest.mock('../screens/Contact/Contact.service', () => {
-  const originalModule = jest.requireActual(
-    '../screens/Contact/Contact.service',
-  );
+  const originalModule = jest.requireActual('../screens/Contact/Contact.service');
   return {
     ...originalModule,
     fetchContacts: jest.fn(),
   };
 });
 
+jest.mock('react-native-localize', () => ({
+  getCountry: jest.fn(() => 'IN'),
+}));
+
 const mockFetchContacts = fetchContacts as jest.Mock;
 const mockAccessToken = 'test-access-token';
 
-describe('test getContactsDetails functionality', () => {
+describe('getContactsDetails', () => {
   const mockContacts: Contact[] = [
     {
       recordID: '2',
       givenName: 'Rekha',
       middleName: 'hii',
       familyName: 'Korepu',
-      phoneNumbers: [{id: '4', label: 'Mobile', number: '+91 86395 23822'}],
+      phoneNumbers: [{ id: '4', label: 'Mobile', number: '+91 8639523822' }],
     } as unknown as Contact,
     {
       recordID: '3',
       givenName: 'Anush',
       middleName: '',
       familyName: '',
-      phoneNumbers: [{id: '6', label: 'Mobile', number: '+91 98495 45903'}],
+      phoneNumbers: [{ id: '6', label: 'Mobile', number: '+91 9849545903' }],
+    } as unknown as Contact,
+    {
+      recordID: '4',
+      givenName: 'Anjani',
+      middleName: '',
+      familyName: '',
+      phoneNumbers: [{ id: '6', label: 'Mobile', number: '' }],
+    } as unknown as Contact,
+    {
+      recordID: '5',
+      givenName: 'Anjaniii',
+      middleName: '',
+      familyName: '',
+      phoneNumbers: [{ id: '6', label: 'Mobile', number: '+91 12345' }],
     } as unknown as Contact,
   ];
 
   const mockResponseData = [
     {
-      mobileNumber: '8639523822',
+      mobileNumber: '+91 86395 23822',
       doesHaveAccount: true,
       profilePicture: 'smartchatpic1.jpg',
     },
     {
-      mobileNumber: '9849545903',
+      mobileNumber: '+91 98495 45903',
       doesHaveAccount: false,
       profilePicture: null,
     },
@@ -56,26 +72,35 @@ describe('test getContactsDetails functionality', () => {
       json: jest.fn().mockResolvedValue(mockResponseData),
     });
 
-    const result: ContactType[] = await getContactsDetails(
-      mockContacts,
-      mockAccessToken,
-    );
+    const result: ContactType[] = await getContactsDetails(mockContacts, mockAccessToken);
 
     expect(mockFetchContacts).toHaveBeenCalledWith(
-      ['8639523822', '9849545903'],
-      mockAccessToken,
+      ['+91 86395 23822', '+91 98495 45903'],
+      mockAccessToken
     );
 
     expect(result).toEqual([
       {
         name: 'Rekha hii Korepu',
-        mobileNumber: '8639523822',
+        mobileNumber: '+91 8639523822',
         doesHaveAccount: true,
         profilePicture: 'smartchatpic1.jpg',
       },
       {
         name: 'Anush',
-        mobileNumber: '9849545903',
+        mobileNumber: '+91 9849545903',
+        doesHaveAccount: false,
+        profilePicture: null,
+      },
+      {
+        name: 'Anjani',
+        mobileNumber: '',
+        doesHaveAccount: false,
+        profilePicture: null,
+      },
+      {
+        name: 'Anjaniii',
+        mobileNumber: '+91 12345',
         doesHaveAccount: false,
         profilePicture: null,
       },
@@ -87,21 +112,35 @@ describe('test getContactsDetails functionality', () => {
       json: jest.fn().mockResolvedValue([]),
     });
 
-    const result: ContactType[] = await getContactsDetails(
-      mockContacts,
-      mockAccessToken,
+    const result: ContactType[] = await getContactsDetails(mockContacts, mockAccessToken);
+
+    expect(mockFetchContacts).toHaveBeenCalledWith(
+      ['+91 86395 23822', '+91 98495 45903'],
+      mockAccessToken
     );
 
     expect(result).toEqual([
       {
         name: 'Rekha hii Korepu',
-        mobileNumber: '8639523822',
+        mobileNumber: '+91 8639523822',
         doesHaveAccount: false,
         profilePicture: null,
       },
       {
         name: 'Anush',
-        mobileNumber: '9849545903',
+        mobileNumber: '+91 9849545903',
+        doesHaveAccount: false,
+        profilePicture: null,
+      },
+      {
+        name: 'Anjani',
+        mobileNumber: '',
+        doesHaveAccount: false,
+        profilePicture: null,
+      },
+      {
+        name: 'Anjaniii',
+        mobileNumber: '+91 12345',
         doesHaveAccount: false,
         profilePicture: null,
       },
