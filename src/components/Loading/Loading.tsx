a86e6354ebebdcfef734abd58d4e470f2fb2e430
SoumyaKatta <soumya.katta@everest.engineering>
2025-05-21 15:29:52 +0530

    Implement the loading indicator component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,24 @@
+import {getStyles} from './Loading.styles.ts';
+import LottieView from 'lottie-react-native';
+import {View} from 'react-native';
+interface LoadingIndicatorProps {
+  visible: boolean;
+}
+const LoadingIndicator: React.FC<LoadingIndicatorProps> = ({visible}) => {
+  const styles = getStyles();
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <View style={styles.overlay} testID="animation">
+      <LottieView
+        source={require('../../../assets/loadingImages/loadingImage.json')}
+        autoPlay
+        loop
+        style={styles.lottieView}
+      />
+    </View>
+  );
+};
+export default LoadingIndicator;
