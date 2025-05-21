80eaf1362b293b9a8cf09256fdbfd551d3c263dd
SoumyaKatta <soumya.katta@everest.engineering>
2025-05-21 15:36:25 +0530

    Render the loading indicator component in contacts screen

--- src/screens/Contact/Contact.tsx
+++ src/screens/Contact/Contact.tsx
@@ -1,22 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import {getStyles} from './Contact.styles';
-import { Alert, FlatList, Text, TouchableOpacity, View} from 'react-native';
+import {Alert, FlatList, Text, TouchableOpacity, View} from 'react-native';
 import {Theme} from '../../utils/themes';
 import {useAppTheme} from '../../hooks/appTheme';
-import { ContactCard } from '../../components/ContactCard/ContactCard.tsx';
-import { useDispatch, useSelector } from 'react-redux';
-import { requestPermission } from '../../permissions/permissions.ts';
+import {ContactCard} from '../../components/ContactCard/ContactCard.tsx';
+import {useDispatch, useSelector} from 'react-redux';
+import {requestPermission} from '../../permissions/permissions.ts';
 import Contacts from 'react-native-contacts';
-import { setContacts } from '../../redux/reducers/contacts.reducer.ts';
-import { storeState } from '../../redux/store.ts';
+import {setContacts} from '../../redux/reducers/contacts.reducer.ts';
+import {storeState} from '../../redux/store.ts';
 import EncryptedStorage from 'react-native-encrypted-storage';
-import { Contact as ContactType } from '../../types/Contacts.ts';
-import { getContactsDetails } from '../../utils/getContactsDetails.ts';
-import { getTokens } from '../../utils/getTokens.ts';
-import LoadingScreen from '../Loading/Loading.tsx';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { RootStackParamList } from '../../types/Navigations.ts';
-
+import {Contact as ContactType} from '../../types/Contacts.ts';
+import {getContactsDetails} from '../../utils/getContactsDetails.ts';
+import {getTokens} from '../../utils/getTokens.ts';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {RootStackParamList} from '../../types/Navigations.ts';
+import LoadingIndicator from '../../components/Loading/Loading.tsx';
 
 export function Contact(): React.JSX.Element {
   const theme: Theme = useAppTheme();
@@ -24,14 +23,15 @@ export function Contact(): React.JSX.Element {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   const [selectedTab, setSelectedTab] = useState('Contacts');
   const dispatch = useDispatch();
-  const contacts = useSelector((state: storeState)=> state.contacts.contacts);
-  const user = useSelector((state: storeState)=> state.user);
+  const contacts = useSelector((state: storeState) => state.contacts.contacts);
+  const user = useSelector((state: storeState) => state.user);
   const [filteredContacts, setFilteredContacts] = useState<ContactType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadContacts = async () => {
       try {
+        setIsLoading(true);
         const hasPermission = await requestPermission('contacts');
         if (!hasPermission) {
           Alert.alert('Permission for contacts was denied');
@@ -42,46 +42,48 @@ export function Contact(): React.JSX.Element {
           await EncryptedStorage.clear();
           navigation.reset({
             index: 0,
-            routes: [{ name: 'WelcomeScreen' }],
+            routes: [{name: 'WelcomeScreen'}],
           });
           return;
         }
-          const deviceContacts = await Contacts.getAll();
-          if(deviceContacts.length === 0 ){
-            dispatch(setContacts([]));
-            return;
-          }
-          const resultantContacts = await getContactsDetails(deviceContacts, tokens.access_token);
-          dispatch(setContacts(resultantContacts));
-        } catch (error) {
-          Alert.alert('Something went wrong while fetching contacts details. Please try again');
-        } finally{
-          setIsLoading(false);
+        const deviceContacts = await Contacts.getAll();
+        if (deviceContacts.length === 0) {
+          dispatch(setContacts([]));
+          return;
         }
+        const resultantContacts = await getContactsDetails(
+          deviceContacts,
+          tokens.access_token,
+        );
+        dispatch(setContacts(resultantContacts));
+      } catch (error) {
+        Alert.alert(
+          'Something went wrong while fetching contacts details. Please try again',
+        );
+      } finally {
+        setIsLoading(false);
+      }
     };
-        loadContacts();
+    loadContacts();
   }, [dispatch, navigation, user.mobileNumber]);
 
-
   useEffect(() => {
-    const filterContacts = (userContacts: ContactType[])=> {
-      if(userContacts.length === 0) {
+    const filterContacts = (userContacts: ContactType[]) => {
+      if (userContacts.length === 0) {
         return [];
       }
       if (selectedTab === 'Contacts') {
-          setFilteredContacts(userContacts.filter(contact => contact.doesHaveAccount));
+        setFilteredContacts(
+          userContacts.filter(contact => contact.doesHaveAccount),
+        );
       } else {
-          setFilteredContacts(userContacts.filter(contact => !contact.doesHaveAccount));
+        setFilteredContacts(
+          userContacts.filter(contact => !contact.doesHaveAccount),
+        );
       }
     };
     filterContacts(contacts);
   }, [contacts, selectedTab]);
-
-  if(isLoading) {
-    return(
-      <LoadingScreen/>
-    );
-  }
 
   return (
     <>
@@ -109,25 +111,39 @@ export function Contact(): React.JSX.Element {
             </Text>
             {selectedTab === 'Invite' && <View style={styles.line} />}
           </TouchableOpacity>
+          <LoadingIndicator visible={isLoading} />
         </View>
         <View style={styles.contactsContainer}>
-              { contacts.length === 0 ? (
-                <View style={styles.messageContainer}><Text style={styles.messageText}>Add your friends to contacts and invite them to SmartChat</Text></View>
-                ) : filteredContacts.length === 0 ? (
-                  selectedTab === 'Contacts' ? (
-                   <View style={styles.messageContainer}><Text style={styles.messageText}>Invite your contacts to SmartChat and start your conversations</Text></View>
-                  ) : (
-                    <View style={styles.messageContainer}><Text style={styles.messageText}>All your contacts are on SmartChat. Continue your conversations with them</Text></View>
-                  )
-                ) : (
-                  <FlatList
-                    data={filteredContacts}
-                    keyExtractor={(item) => item.mobileNumber}
-                    contentContainerStyle={styles.contactsBody}
-                    initialNumToRender={10}
-                    renderItem={({ item }) => <ContactCard contact={item} />}
-                  />
-              )}
+          {contacts.length === 0 ? (
+            <View style={styles.messageContainer}>
+              <Text style={styles.messageText}>
+                Add your friends to contacts and invite them to SmartChat
+              </Text>
+            </View>
+          ) : filteredContacts.length === 0 ? (
+            selectedTab === 'Contacts' ? (
+              <View style={styles.messageContainer}>
+                <Text style={styles.messageText}>
+                  Invite your contacts to SmartChat and start your conversations
+                </Text>
+              </View>
+            ) : (
+              <View style={styles.messageContainer}>
+                <Text style={styles.messageText}>
+                  All your contacts are on SmartChat. Continue your
+                  conversations with them
+                </Text>
+              </View>
+            )
+          ) : (
+            <FlatList
+              data={filteredContacts}
+              keyExtractor={item => item.mobileNumber}
+              contentContainerStyle={styles.contactsBody}
+              initialNumToRender={10}
+              renderItem={({item}) => <ContactCard contact={item} />}
+            />
+          )}
         </View>
       </View>
     </>
