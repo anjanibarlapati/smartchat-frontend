c03d87ee8ac4a08314f309d93aa4e7a9994960e9
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-22 17:46:43 +0530

    Implement message reducer for storing messages

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './reducers/contacts.reducer';
+import messagesReducer from './reducers/messages.reducer';
 import { themeReducer } from './reducers/theme.reducer';
 import { userReducer } from './reducers/user.reducer';
 
 
+
 export const store = configureStore({
     reducer: {
-       user: userReducer,
-       theme: themeReducer,
-       contacts: contactsReducer,
+        user: userReducer,
+        theme: themeReducer,
+        contacts: contactsReducer,
+        messages: messagesReducer,
     },
 });
 
 export type storeState = ReturnType<typeof store.getState>;
-
 
