87d570a1c7c60bcb230679ce53c472c9e906f08f
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-22 17:49:41 +0530

    Read newMessage event from socket and decrypt the message

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,40 +1,66 @@
-import {io} from 'socket.io-client';
-import {BASE_URL} from './constants';
+
+import { DefaultEventsMap } from '@socket.io/component-emitter';
 import EncryptedStorage from 'react-native-encrypted-storage';
+import { io, Socket } from 'socket.io-client';
+import { addMessage } from '../redux/reducers/messages.reducer';
+import { store } from '../redux/store';
+import { BASE_URL } from './constants';
+import { decryptMessage } from './decryptMessage';
 
-let socket : any;
-export const socketConnection = async () => {
+
+let socket: Socket<DefaultEventsMap, DefaultEventsMap> | null = null;
+
+export const socketConnection = async (mobileNumber: string) => {
+
   try {
-    const token = await EncryptedStorage.getItem('token');
-    if (token) {
-      const tokenData = JSON.parse(token);
+    const generateId = () => `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+    const token = await EncryptedStorage.getItem(mobileNumber);
+    if (!token) { return; }
 
-      if (tokenData.access_token) {
-         socket = io(BASE_URL, {
-          transports: ['websocket'],
-        });
-        socket.on('connect', () => {
-          console.log('socket connection', socket.connected);
-          console.log('socket id is', socket.id);
-        });
-        socket.on('disconnect', () => {
-          console.log('socket disconnected');
-        });
-      }
+    const tokenData = JSON.parse(token);
+    if (socket && socket.connected) { return; }
+
+    if (tokenData.access_token) {
+      socket = io(BASE_URL, { transports: ['websocket'] });
+
+      socket.on('connect', () => {
+        socket?.emit('register', mobileNumber);
+      });
+
+      socket.on('newMessage', async (data) => {
+
+        const actualMessage = await decryptMessage(
+          data.senderMobileNumber,
+          data.message,
+          data.nonce,
+          tokenData.access_token
+        );
+        const structuredMsg: any = {
+          id: generateId(),
+          sender: data.senderMobileNumber,
+          receiver: data.receiverMobileNumber,
+          message: actualMessage,
+          sentAt: data.sentAt,
+          isSender: false,
+        };
+        store.dispatch(addMessage({ chatId: data.senderMobileNumber, message: structuredMsg }));
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Socket disconnected');
+      });
     }
   } catch (error) {
-    console.log(error);
+    throw new Error('Unable to establish socket connection');
   }
 };
 
-export const socketDisconnect = async () =>{
+
+export const socketDisconnect = () => {
   if (socket) {
+    socket.removeAllListeners();
     socket.disconnect();
     socket = null;
     console.log('Socket manually disconnected');
   }
 };
-
-
-
-
