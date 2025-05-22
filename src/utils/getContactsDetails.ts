fbad4d9d6d4e394e78e523ad9085ab8f53e584fd
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-22 17:57:05 +0530

    Export normalizeNumber function in getContactDetails

--- src/utils/getContactsDetails.ts
+++ src/utils/getContactsDetails.ts
@@ -6,49 +6,50 @@ import { Contact as ContactType, UserContact } from '../types/Contacts';
 
 const defaultCountryCode = RNLocalize.getCountry() as CountryCode || 'IN';
 
-function normalizeNumber(number: string): string | null {
-    if (!number) {
-      return null;
-    }
-    const parsedNumber = parsePhoneNumberFromString(number, defaultCountryCode);
-    if (!parsedNumber || !parsedNumber.isValid()) {
-      return null;
-    }
-    return parsedNumber.formatInternational();
+export function normalizeNumber(number: string): string | null {
+  if (!number) {
+    return null;
+  }
+  const parsedNumber = parsePhoneNumberFromString(number, defaultCountryCode);
+  if (!parsedNumber || !parsedNumber.isValid()) {
+    return null;
+  }
+  return parsedNumber.formatInternational();
 }
 
 
 export const getContactsDetails = async (contacts: Contact[], access_token: string) => {
-    try {
-        const deviceContacts = contacts.flatMap(contact => {
-        const name = `${contact.givenName} ${contact.middleName} ${contact.familyName}`.trim();
-        return (contact.phoneNumbers || []).map(phone => {
-            const normalizedPhoneNumber = normalizeNumber(phone.number);
-            return {
-              name,
-              originalNumber: phone.number,
-              mobileNumber: normalizedPhoneNumber,
-            };
-        });
+  try {
+    const deviceContacts = contacts.flatMap(contact => {
+      const name = `${contact.givenName} ${contact.middleName} ${contact.familyName}`.trim();
+      return (contact.phoneNumbers || []).map(phone => {
+        const normalizedPhoneNumber = normalizeNumber(phone.number);
+        return {
+          name,
+          originalNumber: phone.number,
+          mobileNumber: normalizedPhoneNumber,
+        };
       });
+    });
 
-      const phoneNumbers = deviceContacts.map(contact => contact.mobileNumber).filter((mobileNumber): mobileNumber is string => mobileNumber !== null && mobileNumber !== undefined);
+    const phoneNumbers = deviceContacts.map(contact => contact.mobileNumber).filter((mobileNumber): mobileNumber is string => mobileNumber !== null && mobileNumber !== undefined);
 
 
-      const response = await fetchContacts(phoneNumbers, access_token);
-      const result = await response.json();
+    const response = await fetchContacts(phoneNumbers, access_token);
+    const result = await response.json();
 
-      const resultantContacts: ContactType[] = deviceContacts.map(contact => {
-          const data = result.find( (resultantContact: UserContact) => resultantContact.mobileNumber === contact.mobileNumber);
-          return {
-            name: contact.name,
-            mobileNumber: contact.originalNumber,
-            doesHaveAccount: data ? data.doesHaveAccount : false,
-            profilePicture: data ? data.profilePicture : null,
-          };
-      });
-      return resultantContacts;
-    } catch(error) {
-      throw new Error('Something went wrong while fetching contacts details');
-    }
+
+    const resultantContacts: ContactType[] = deviceContacts.map(contact => {
+      const data = result.find((resultantContact: UserContact) => resultantContact.mobileNumber === contact.mobileNumber);
+      return {
+        name: contact.name,
+        mobileNumber: contact.originalNumber,
+        doesHaveAccount: data ? data.doesHaveAccount : false,
+        profilePicture: data ? data.profilePicture : null,
+      };
+    });
+    return resultantContacts;
+  } catch (error) {
+    throw new Error('Something went wrong while fetching contacts details');
+  }
 };
