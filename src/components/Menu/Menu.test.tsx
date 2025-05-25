4233340d014332479304ab0eac0b7d53d26d9b33
MamathaNiyal23 <mamatha.niyal@everest.engineering>
2025-05-25 11:59:42 +0530

    Update testcase for chat options modal

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,57 +1,71 @@
-import { render, screen, fireEvent } from '@testing-library/react-native';
-import { Menu } from './Menu';
-import { Provider } from 'react-redux';
-import { store } from '../../redux/store';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
+import {Provider} from 'react-redux';
+import {store} from '../../redux/store';
+import {Menu} from './Menu';
 
 describe('Tests related to Menu component', () => {
   it('should render the menu image', () => {
     render(
       <Provider store={store}>
         <Menu />
-      </Provider>
+      </Provider>,
     );
     expect(screen.getByLabelText('Menu-Image')).toBeTruthy();
   });
 
-  it('should close the modal when "Clear Chat" is pressed', () => {
+  it('should close the modal when "Clear Chat" is pressed', async () => {
     render(
       <Provider store={store}>
         <Menu />
-      </Provider>
+      </Provider>,
     );
 
     fireEvent.press(screen.getByLabelText('Menu-Image'));
     expect(screen.getByText('Clear Chat')).toBeTruthy();
 
     fireEvent.press(screen.getByText('Clear Chat'));
-    expect(screen.queryByText('Clear Chat')).toBeNull();
+    fireEvent.press(screen.getByText('Yes'));
+    await waitFor(() => {
+      expect(screen.queryByText('Clear Chat')).toBeNull();
+    });
   });
 
-  it('should close the modal when "Block" is pressed', () => {
+  it('should close the modal when "Block" is pressed', async () => {
     render(
       <Provider store={store}>
         <Menu />
-      </Provider>
+      </Provider>,
     );
 
     fireEvent.press(screen.getByLabelText('Menu-Image'));
     expect(screen.getByText('Block')).toBeTruthy();
 
     fireEvent.press(screen.getByText('Block'));
-    expect(screen.queryByText('Block')).toBeNull();
+    fireEvent.press(screen.getByText('Yes'));
+
+    await waitFor(() => {
+      expect(screen.queryByText('Block')).toBeNull();
+    });
   });
 
-  it('should close the modal when clicking outside the modal (overlay)', () => {
+  it('should close the modal when clicking outside the modal (overlay)', async () => {
     render(
       <Provider store={store}>
         <Menu />
-      </Provider>
+      </Provider>,
     );
 
     fireEvent.press(screen.getByLabelText('Menu-Image'));
     expect(screen.getByText('Clear Chat')).toBeTruthy();
 
     fireEvent.press(screen.getByLabelText('overlay'));
-    expect(screen.queryByText('Clear Chat')).toBeNull();
+    await waitFor(() => {
+      expect(screen.queryByText('Clear Chat')).toBeNull();
+    });
   });
 });
