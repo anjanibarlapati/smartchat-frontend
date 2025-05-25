3b92a50b408f9d1776e4e2ee4d27672855e20f72
MamathaNiyal23 <mamatha.niyal@everest.engineering>
2025-05-25 11:58:58 +0530

    Add alert modals for clear and block options in individual chat screen

--- src/components/ChatOptionsModal/ChatOptionsModal.tsx
+++ src/components/ChatOptionsModal/ChatOptionsModal.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  View,
-  Text,
   Modal,
+  Platform,
+  Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
-  Platform,
+  View,
 } from 'react-native';
-import {Theme} from '../../utils/themes';
 import {useAppTheme} from '../../hooks/appTheme';
-import { getStyles } from './ChatOptionsModal.styles';
+import {Theme} from '../../utils/themes';
+import {AlertModal} from '../AlertModal/AlertModal';
+import {getStyles} from './ChatOptionsModal.styles';
 
 type ChatBlockModalProps = {
   visible: boolean;
@@ -26,25 +27,58 @@ export const ChatOptionsModal = ({
 }: ChatBlockModalProps) => {
   const theme: Theme = useAppTheme();
   const styles = getStyles(theme);
+
+  const [showClearAlert, setShowClearAlert] = useState(false);
+  const [showBlockAlert, setShowBlockAlert] = useState(false);
+
   return (
-    <Modal
-      transparent
-      animationType="fade"
-      visible={visible}
-      onRequestClose={onClose}
-      >
-      <TouchableWithoutFeedback onPress={onClose} >
-        <View style={Platform.OS === 'android' ? [styles.overlay, {paddingTop: 50}] : [styles.overlay, {paddingTop: 101}]}  accessibilityLabel="overlay">
-            <View style={styles.modalContainer} >
-              <TouchableOpacity onPress={onClearChat}>
+    <>
+      <Modal
+        transparent
+        animationType="fade"
+        visible={visible}
+        onRequestClose={onClose}>
+        <TouchableWithoutFeedback onPress={onClose}>
+          <View
+            style={
+              Platform.OS === 'android'
+                ? [styles.overlay, {paddingTop: 50}]
+                : [styles.overlay, {paddingTop: 101}]
+            }
+            accessibilityLabel="overlay">
+            <View style={styles.modalContainer}>
+              <TouchableOpacity onPress={() => setShowClearAlert(true)}>
                 <Text style={styles.text}>Clear Chat</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={onBlock}>
+              <TouchableOpacity onPress={() => setShowBlockAlert(true)}>
                 <Text style={styles.text}>Block</Text>
               </TouchableOpacity>
             </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </Modal>
+          </View>
+        </TouchableWithoutFeedback>
+        <AlertModal
+          message={'Do you really want to clear this chat?'}
+          visible={showClearAlert}
+          confirmText={'Yes'}
+          cancelText={'Cancel'}
+          onConfirm={() => {
+            setShowClearAlert(false);
+            onClearChat();
+          }}
+          onCancel={() => setShowClearAlert(false)}
+        />
+        <AlertModal
+          message={'Are you sure you want to block this chat?'}
+          visible={showBlockAlert}
+          confirmText={'Yes'}
+          cancelText={'Cancel'}
+          onConfirm={() => {
+            setShowBlockAlert(false);
+            onBlock();
+          }}
+          onCancel={() => setShowBlockAlert(false)}
+        />
+      </Modal>
+    </>
   );
 };
