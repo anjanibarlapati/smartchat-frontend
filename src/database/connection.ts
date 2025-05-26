2ec3907b452afa661e808a84b3b334fafa2a8c99
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-26 15:42:06 +0530

    Add functions to open and close sqlite database

---
+++ src/database/connection.ts
@@ -0,0 +1,26 @@
+import SQLite from 'react-native-sqlite-storage';
+
+SQLite.enablePromise(true);
+SQLite.DEBUG(true);
+
+
+export const getDBConnection = async () => {
+  try {
+    const db = await SQLite.openDatabase({
+      name: 'smartchat.db',
+      location: 'default',
+    });
+    return db;
+  } catch (error) {
+    throw new Error('Failed to open database');
+  }
+};
+
+export const closeConnection = async (db: SQLite.SQLiteDatabase) => {
+    try{
+    return db.close();
+  } catch(error){
+    throw new Error('Failed to close database');
+  }
+};
+
