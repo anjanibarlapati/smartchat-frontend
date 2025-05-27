618660454605888299b3fc5112bfd71142d2f011
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-27 15:39:39 +0530

    Implemennt handle clear chat functionality and update messages state accordingly

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,12 +2,35 @@ import {Image, TouchableOpacity, View} from 'react-native';
 import {styles} from './Menu.styles';
 import {ChatOptionsModal} from '../ChatOptionsModal/ChatOptionsModal';
 import {useState} from 'react';
+import {clearUserChat} from '../ChatOptionsModal/clearChat.service';
+import {useDispatch, useSelector} from 'react-redux';
+import {storeState} from '../../redux/store';
+import {clearUserMessages} from '../../redux/reducers/messages.reducer';
+import {useAlertModal} from '../../hooks/useAlertModal';
 
-export const Menu = () => {
+export const Menu = ({
+  receiverMobileNumber,
+}: {
+  receiverMobileNumber: string;
+}) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const handleClearChat = () => {
-    setIsModalVisible(false);
+  const dispatch = useDispatch();
+  const {showAlert} = useAlertModal();
+  const user = useSelector((state: storeState) => state.user);
+  const handleClearChat = async () => {
+    try {
+      const response = await clearUserChat(
+        user.mobileNumber,
+        receiverMobileNumber,
+      );
+      if (response.ok) {
+        dispatch(clearUserMessages({chatId: receiverMobileNumber}));
+      }
+    } catch (error) {
+      showAlert('Something went wrong. Please try again', 'error');
+    } finally {
+      setIsModalVisible(false);
+    }
   };
 
   const handleBlock = () => {
