ba5256dc41f7ab5d36cdf9237f482d8c799e4717
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-05-27 15:38:15 +0530

    Add clear chat api integration test cases

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -7,65 +7,123 @@ import {
 import {Provider} from 'react-redux';
 import {store} from '../../redux/store';
 import {Menu} from './Menu';
+import {clearUserChat} from '../ChatOptionsModal/clearChat.service';
 
-describe('Tests related to Menu component', () => {
-  it('should render the menu image', () => {
-    render(
-      <Provider store={store}>
-        <Menu />
-      </Provider>,
-    );
-    expect(screen.getByLabelText('Menu-Image')).toBeTruthy();
-  });
+const receiverMobileNumber = '9812345098';
+jest.mock('../ChatOptionsModal/clearChat.service', () => ({
+  clearUserChat: jest.fn(),
+}));
+jest.mock('../../hooks/useAlertModal', () => ({
+  useAlertModal: jest.fn(),
+}));
+const mockShowAlert = jest.fn();
 
-  it('should close the modal when "Clear Chat" is pressed', async () => {
-    render(
-      <Provider store={store}>
-        <Menu />
-      </Provider>,
-    );
+jest.mock('react-native-encrypted-storage', () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  clear: jest.fn(),
+}));
+const renderMenu = () => {
+  return render(
+    <Provider store={store}>
+      <Menu receiverMobileNumber={receiverMobileNumber} />
+    </Provider>,
+  );
+};
 
-    fireEvent.press(screen.getByLabelText('Menu-Image'));
-    expect(screen.getByText('Clear Chat')).toBeTruthy();
-
-    fireEvent.press(screen.getByText('Clear Chat'));
-    fireEvent.press(screen.getByText('Yes'));
-    await waitFor(() => {
-      expect(screen.queryByText('Clear Chat')).toBeNull();
+describe('Should render Menu component', () => {
+  beforeAll(() => {
+    (
+      require('../../hooks/useAlertModal').useAlertModal as jest.Mock
+    ).mockReturnValue({
+      showAlert: mockShowAlert,
     });
   });
-
+  it('should render the menu image', () => {
+    const {getByLabelText} = renderMenu();
+    expect(getByLabelText('Menu-Image')).toBeTruthy();
+  });
   it('should close the modal when "Block" is pressed', async () => {
-    render(
-      <Provider store={store}>
-        <Menu />
-      </Provider>,
-    );
-
+    const {getByText, queryByText} = renderMenu();
     fireEvent.press(screen.getByLabelText('Menu-Image'));
-    expect(screen.getByText('Block')).toBeTruthy();
+    expect(getByText('Block')).toBeTruthy();
 
-    fireEvent.press(screen.getByText('Block'));
-    fireEvent.press(screen.getByText('Yes'));
+    fireEvent.press(getByText('Block'));
+    fireEvent.press(getByText('Yes'));
 
     await waitFor(() => {
-      expect(screen.queryByText('Block')).toBeNull();
+      expect(queryByText('Block')).toBeNull();
     });
   });
 
   it('should close the modal when clicking outside the modal (overlay)', async () => {
-    render(
-      <Provider store={store}>
-        <Menu />
-      </Provider>,
-    );
+    const {getByText, queryByText, getByLabelText} = renderMenu();
 
-    fireEvent.press(screen.getByLabelText('Menu-Image'));
-    expect(screen.getByText('Clear Chat')).toBeTruthy();
+    fireEvent.press(getByLabelText('Menu-Image'));
+    expect(getByText('Clear Chat')).toBeTruthy();
 
-    fireEvent.press(screen.getByLabelText('overlay'));
+    fireEvent.press(getByLabelText('overlay'));
     await waitFor(() => {
-      expect(screen.queryByText('Clear Chat')).toBeNull();
+      expect(queryByText('Clear Chat')).toBeNull();
+    });
+  });
+  it('should close the modal when "Clear Chat" is pressed', async () => {
+    (clearUserChat as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue({message: 'Cleared chat successfully'}),
+    });
+
+    const {getByLabelText, getByText} = renderMenu();
+
+    fireEvent.press(getByLabelText('Menu-Image'));
+    expect(getByText('Clear Chat')).toBeTruthy();
+
+    fireEvent.press(getByText('Clear Chat'));
+    fireEvent.press(getByText('Yes'));
+    await waitFor(() => {
+      expect(clearUserChat).toHaveBeenCalledWith(
+        expect.any(String),
+        receiverMobileNumber,
+      );
+    });
+  });
+
+  it('should show Alert if there is any error', async () => {
+    (clearUserChat as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue({message: 'Cleared chat successfully'}),
+    });
+
+    const {getByLabelText, getByText} = renderMenu();
+
+    fireEvent.press(getByLabelText('Menu-Image'));
+    expect(getByText('Clear Chat')).toBeTruthy();
+
+    fireEvent.press(getByText('Clear Chat'));
+    fireEvent.press(getByText('Yes'));
+    await waitFor(() => {
+      expect(clearUserChat).toHaveBeenCalledWith(
+        expect.any(String),
+        receiverMobileNumber,
+      );
+    });
+  });
+
+  it('should show an alert when clearUserChat fails', async () => {
+    (clearUserChat as jest.Mock).mockRejectedValue(new Error('API failure'));
+    const {getByLabelText, getByText} = renderMenu();
+
+    fireEvent.press(getByLabelText('Menu-Image'));
+    fireEvent.press(getByText('Clear Chat'));
+    fireEvent.press(getByText('Yes'));
+
+    await waitFor(() => {
+      expect(clearUserChat).toHaveBeenCalled();
+
+      expect(mockShowAlert).toHaveBeenCalledWith(
+        'Something went wrong. Please try again',
+        'error',
+      );
     });
   });
 });
