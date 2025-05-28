e8e831ff80ea7ed02a3feeeb27f9cdcb7d06e241
MamathaNiyal23 <mamatha.niyal@everest.engineering>
2025-05-28 16:06:22 +0530

    Update socket file to read 'messageDelivered' and 'messageRead' events

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,49 +1,79 @@
-
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import { io, Socket } from 'socket.io-client';
-import { addMessage } from '../redux/reducers/messages.reducer';
+import {
+  addMessage,
+  updateMessageStatus,
+} from '../redux/reducers/messages.reducer';
 import { store } from '../redux/store';
 import { BASE_URL } from './constants';
 import { decryptMessage } from './decryptMessage';
 
-
 let socket: Socket<DefaultEventsMap, DefaultEventsMap> | null = null;
 
 export const socketConnection = async (mobileNumber: string) => {
-
   try {
-    const generateId = () => `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
     const token = await EncryptedStorage.getItem(mobileNumber);
-    if (!token) { return; }
+    if (!token) {
+      return;
+    }
     const tokenData = JSON.parse(token);
     await socketDisconnect();
 
     if (tokenData.access_token) {
-      socket = io(BASE_URL, { transports: ['websocket']});
+      socket = io(BASE_URL, {transports: ['websocket']});
 
       socket.on('connect', () => {
         socket?.emit('register', mobileNumber);
       });
 
-      socket.on('newMessage', async (data) => {
+      socket.on('newMessage', async data => {
         const actualMessage = await decryptMessage(
           data.senderMobileNumber,
           data.message,
           data.nonce,
-          tokenData.access_token
+          tokenData.access_token,
         );
         const structuredMsg: any = {
-          id: generateId(),
+          id: data.id,
           sender: data.senderMobileNumber,
           receiver: data.receiverMobileNumber,
           message: actualMessage,
           sentAt: data.sentAt,
           isSender: false,
+          status: data.status,
         };
-        store.dispatch(addMessage({ chatId: data.senderMobileNumber, message: structuredMsg }));
+
+        store.dispatch(
+          addMessage({chatId: data.senderMobileNumber, message: structuredMsg}),
+        );
       });
 
+      socket.on('messageDelivered', data => {
+        const {messageId, receiverMobileNumber, status} = data;
+        setTimeout(() => {
+          store.dispatch(
+            updateMessageStatus({
+              chatId: receiverMobileNumber,
+              id: messageId,
+              status: status,
+            }),
+          );
+        }, 300);
+      });
+
+      socket.on('messageRead', data => {
+        const {messageId, chatId, status} = data;
+        setTimeout(() => {
+          store.dispatch(
+            updateMessageStatus({
+              chatId,
+              id: messageId,
+              status,
+            }),
+          );
+        }, 400);
+      });
       socket.on('disconnect', () => {
         console.log('Socket disconnected');
       });
@@ -53,8 +83,7 @@ export const socketConnection = async (mobileNumber: string) => {
   }
 };
 
-
-export const socketDisconnect = async () => {
+export const socketDisconnect = () => {
   if (socket) {
     socket.removeAllListeners();
     socket.disconnect();
@@ -62,3 +91,5 @@ export const socketDisconnect = async () => {
     console.log('Socket manually disconnected');
   }
 };
+
+export const getSocket = () => socket;
