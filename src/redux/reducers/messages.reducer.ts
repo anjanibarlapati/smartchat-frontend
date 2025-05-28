389ef43f85d2f6dc75f54e0a63749daad892c3b4
MamathaNiyal23 <mamatha.niyal@everest.engineering>
2025-05-28 16:04:26 +0530

    Update the message slice to include updateMessageStatus in the reducer

--- src/redux/reducers/messages.reducer.ts
+++ src/redux/reducers/messages.reducer.ts
@@ -1,40 +1,65 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { storeState } from '../store';
-import {  createSelector } from 'reselect';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createSelector} from 'reselect';
+import {storeState} from '../store';
 
 export interface Message {
-    id: string;
-    sender: string;
-    receiver: string;
-    message: string;
-    sentAt: string;
-    isSender: boolean;
-    status: 'sent' | 'delivered' | 'failed';
+  id: string;
+  sender: string;
+  receiver: string;
+  message: string;
+  sentAt: string;
+  isSender: boolean;
+  status: 'sent' | 'delivered' | 'failed' | 'seen';
 }
 
 interface MessagesState {
-    [chatId: string]: Message[];
+  [chatId: string]: Message[];
 }
 
 const initialState: MessagesState = {};
 
 const messagesSlice = createSlice({
-    name: 'messages',
-    initialState,
-    reducers: {
-        addMessage(state, action: PayloadAction<{ chatId: string; message: Message }>) {
-            const { chatId, message } = action.payload;
-            if (!state[chatId]) { state[chatId] = []; }
-            state[chatId].push(message);
-        },
+  name: 'messages',
+  initialState,
+  reducers: {
+    addMessage(
+      state,
+      action: PayloadAction<{chatId: string; message: Message}>,
+    ) {
+      const {chatId, message} = action.payload;
+
+      if (!state[chatId]) {
+        state[chatId] = [];
+      }
+      state[chatId].push(message);
     },
+
+    updateMessageStatus(
+      state,
+      action: PayloadAction<{
+        chatId: string;
+        id: string;
+        status: 'sent' | 'delivered' | 'failed' | 'seen';
+      }>,
+    ) {
+      const {chatId, id, status} = action.payload;
+
+      const messages = state[chatId];
+
+      if (messages) {
+        const msg = messages.find(m => m.id === id);
+        if (msg) {
+          msg.status = status;
+        }
+      }
+    },
+  },
 });
 
 export const selectMessages = (state: storeState) => state.messages;
 
 export const selectMessagesByChatId = (chatId: string) =>
-  createSelector([selectMessages], (messages) => messages[chatId] || []);
+  createSelector([selectMessages], messages => messages[chatId] || []);
 
-
-export const { addMessage } = messagesSlice.actions;
+export const {addMessage, updateMessageStatus} = messagesSlice.actions;
 export default messagesSlice.reducer;
