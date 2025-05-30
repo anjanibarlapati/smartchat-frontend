a6e5bcd996f85db2e39816ececa421811857f55e
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-05-30 16:57:14 +0530

    Update input chat box component to emit a socket event for self chat case

--- src/components/InputChatBox/InputChatBox.tsx
+++ src/components/InputChatBox/InputChatBox.tsx
@@ -13,38 +13,24 @@ import {storeState} from '../../redux/store';
 import {Theme} from '../../utils/themes';
 import {sendMessage} from './InputChatBox.service';
 import {ChatInputStyles} from './InputChatBox.styles';
+import { getSocket } from '../../utils/socket';
 
-interface InputChatBoxProps {
-  receiverMobileNumber: string;
-  onSendMessage: (text: string) => void;
-}
-export function InputChatBox({
-  receiverMobileNumber,
-  onSendMessage,
-}: InputChatBoxProps) {
+export function InputChatBox({receiverMobileNumber}: {receiverMobileNumber: string}) {
   const theme: Theme = useAppTheme();
   const styles = ChatInputStyles(theme);
-
   const [message, setMessage] = useState('');
   const dispatch = useDispatch();
   const user = useSelector((state: storeState) => state.user);
-  const sendTextMessage = () => {
-    onSendMessage(message);
-    setMessage('');
-  };
-
   const handleSend = async () => {
     if (message.trim() === '') {
       return;
     }
-    sendTextMessage();
     try {
       const result = await sendMessage(
         user.mobileNumber,
         receiverMobileNumber,
         message.trim(),
       );
-
       const msg: Message = {
         id: result.message._id,
         sender: user.mobileNumber,
@@ -55,8 +41,21 @@ export function InputChatBox({
         status: 'sent',
       };
       dispatch(addMessage({chatId: receiverMobileNumber, message: msg}));
+      const socket = await getSocket();
+      if (!socket?.connected) {
+        return;
+      }
+      if(receiverMobileNumber === user.mobileNumber) {
+        socket.emit('messageRead', {
+            messageId: msg.id,
+            chatId: msg.sender,
+          });
+      }
+
     } catch (error) {
-      throw new Error('unable to encrypt message');
+      throw new Error('Unable to encrypt message');
+    } finally{
+       setMessage('');
     }
   };
 
