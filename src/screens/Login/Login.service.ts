ab40fff14e3642b995fc53de4d968bd9ed0bbeea
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-01 14:57:40 +0530

    Add function to fetch user chats and format messages

--- src/screens/Login/Login.service.ts
+++ src/screens/Login/Login.service.ts
@@ -1,6 +1,8 @@
 import { getDeviceId } from 'react-native-device-info';
 import { Credentials } from '../../types/Credentials';
+import { Chat, Messages } from '../../types/message';
 import { BASE_URL } from '../../utils/constants';
+import { decryptMessage } from '../../utils/decryptMessage';
 
 export const login = async(credentials: Credentials) => {
     const response = await fetch(`${BASE_URL}login`, {
@@ -11,4 +13,40 @@ export const login = async(credentials: Credentials) => {
         body: JSON.stringify({...credentials, deviceId: getDeviceId()}),
     });
     return response;
+};
+
+export const fetchChats = async(mobileNumber: string, accessToken: string) => {
+    const response = await fetch(`${BASE_URL}user/chats`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'smart-chat-token-header-key': `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({mobileNumber: mobileNumber}),
+    });
+    return response;
+};
+
+export const formatMessages = async(chats: Chat[], accessToken: string) => {
+  const chatMessages: Messages = {};
+  for (const chat of chats) {
+    const { chatId, messages } = chat;
+    const formattedMessages = [];
+    for (const message of messages) {
+      const actualMessage = await decryptMessage(
+        chatId,
+        message.message,
+        message.nonce,
+        accessToken
+      );
+      formattedMessages.push({
+        message: actualMessage,
+        sentAt: message.sentAt,
+        isSender: message.isSender,
+        status: message.status,
+      });
+    }
+    chatMessages[chatId] = formattedMessages;
+  }
+  return chatMessages;
 };
