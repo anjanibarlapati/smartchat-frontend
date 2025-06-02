7c29142c1aa12c8aefb6530924252e39180358de
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-02 12:12:23 +0530

    Define chat schema

---
+++ src/realm-database/schemas/Chat.ts
@@ -0,0 +1,25 @@
+import Realm from 'realm';
+import { Message } from './Message';
+
+export class Chat extends Realm.Object<Chat> {
+  chatId!: string;
+  isBlocked!: boolean;
+  publicKey!: string | null;
+  messages!: Realm.List<Message>;
+
+  static schema: Realm.ObjectSchema = {
+    name: 'Chat',
+    primaryKey: 'chatId',
+    properties: {
+      chatId: 'string',
+      isBlocked: { type: 'bool', default: false },
+      publicKey: 'string?',
+      messages: {
+        type: 'linkingObjects',
+        objectType: 'Message',
+        property: 'chat',
+      },
+    },
+  };
+}
+
