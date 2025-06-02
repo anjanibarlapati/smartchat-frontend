e8022c6a6c1f6e7e01831d33c3c5930eb36518e1
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-02 13:04:55 +0530

    Update send message service function

--- src/components/InputChatBox/InputChatBox.service.ts
+++ src/components/InputChatBox/InputChatBox.service.ts
@@ -3,22 +3,23 @@ import { getTokens } from '../../utils/getTokens';
 import { getSocket } from '../../utils/socket';
 
 
-export const sendMessage = async (senderMobileNumber: string, receiverMobileNumber: string, message: string, sentAt: string)=> {
+export const sendMessage = async (senderMobileNumber: string, receiverMobileNumber: string, message: string, sentAt: string, status: 'sent' | 'delivered' | 'seen')=> {
   try{
-  const tokens = await getTokens(senderMobileNumber);
-   const { ciphertext, nonce } = await encryptMessage(receiverMobileNumber, message, tokens.access_token);
-    const socket = getSocket();
-    if(!socket){
-      return false;
-    }
-   socket.emit('store-message',{
-      senderMobileNumber: senderMobileNumber,
-      receiverMobileNumber,
-      message: ciphertext,
-      nonce,
-      sentAt,
-   });
+    const tokens = await getTokens(senderMobileNumber);
+    const { ciphertext, nonce } = await encryptMessage(receiverMobileNumber, message, tokens.access_token);
+      const socket = getSocket();
+      if(!socket){
+        return false;
+      }
+    socket.emit('store-message',{
+        senderMobileNumber: senderMobileNumber,
+        receiverMobileNumber,
+        message: ciphertext,
+        nonce,
+        sentAt,
+        status,
+    });
   }catch(error){
-    throw new Error('Unable to emit message to server');
+      throw new Error('Unable to emit message to server');
   }
 };
