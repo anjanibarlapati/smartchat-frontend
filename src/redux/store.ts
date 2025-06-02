982bbc7f3cc3abcb7f4b88314e925cc829769aaa
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-02 12:40:43 +0530

    Update redux after removing message reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './reducers/auth.reducer';
-import messagesReducer from './reducers/messages.reducer';
 import { themeReducer } from './reducers/theme.reducer';
 import { userReducer } from './reducers/user.reducer';
-
-
 
 export const store = configureStore({
     reducer: {
        user: userReducer,
        theme: themeReducer,
        auth: authReducer,
-       messages: messagesReducer,
-
     },
 });
 
