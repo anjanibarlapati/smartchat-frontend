c2d50b06262d2a0983e879622d15959d846f7c7b
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-06-03 10:47:23 +0530

    Remove app folder from jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,6 @@ const config: Config.InitialOptions = {
     '/node_modules/',
     '/assets/',
     '__tests__/',
-    'src/App/App.tsx',
   ],
   transformIgnorePatterns: [
       'node_modules/(?!@react-native|react-native|@react-native/js-polyfills|react-native-image-crop-picker|@react-navigation|react-native-permissions|react-native-contacts|@react-native-community/netinfo)',
