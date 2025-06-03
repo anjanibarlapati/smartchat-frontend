281eed677b3fda062a76db4613eef29cce1fc6f8
varun-martha <varunkumar.martha@everest.engineering>
2025-06-04 02:54:48 +0530

    Modify styles for IndividualChat screen

--- src/screens/IndividualChat/IndividualChat.styles.ts
+++ src/screens/IndividualChat/IndividualChat.styles.ts
@@ -4,11 +4,31 @@ import { Theme } from '../../utils/themes';
 export const getStyles = (theme: Theme) => StyleSheet.create({
     headerStyle: {
         backgroundColor: theme.primaryBackground,
+        paddingRight: 20,
     },
     container: {
         flex: 1,
         backgroundColor: theme.primaryBackground,
         padding: 8,
+    },
+    box: {
+      padding: 16,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: theme.secondaryBackground,
+    },
+    blockedText: {
+      color: theme.secondaryTextColor,
+      fontSize: 14,
+      textAlign: 'center',
+    },
+    headerBox: {
+        backgroundColor: theme.primaryBackground,
+        width: '90%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
 
 });
