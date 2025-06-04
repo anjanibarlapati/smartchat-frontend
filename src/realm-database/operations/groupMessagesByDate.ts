b63e7ad1dd99c23d8ca44451f9fc73673084cc2c
SoumyaKatta <soumya.katta@everest.engineering>
2025-06-04 14:19:37 +0530

    Modify the groupMessagesByDate function

--- src/realm-database/operations/groupMessagesByDate.ts
+++ src/realm-database/operations/groupMessagesByDate.ts
@@ -1,27 +1,8 @@
-import {format, isToday, isYesterday} from 'date-fns';
 import {Message} from '../schemas/Message';
 
 export const groupMessagesByDate = (messages: Realm.Results<Message>) => {
   const grouped: {[key: string]: Message[]} = {};
 
-  for (let i = 0; i < messages.length; i++) {
-    const msg = messages[i];
-    const date = msg.sentAt;
-    let key = format(date, 'yyyy-MM-dd');
-
-    if (isToday(date)) {
-      key = 'Today';
-    } else if (isYesterday(date)) {
-      key = 'Yesterday';
-    }
-
-    if (!grouped[key]) {
-      grouped[key] = [];
-    }
-    grouped[key].push(msg);
-  }
 
   return grouped;
 };
-
-
