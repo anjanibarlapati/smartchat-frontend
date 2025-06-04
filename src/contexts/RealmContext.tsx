bc54d349645aab5795bfa542c4a87e6a8dadec6a
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-04 19:57:53 +0530

    Update realm context

--- src/contexts/RealmContext.tsx
+++ src/contexts/RealmContext.tsx
@@ -1,11 +1,15 @@
 import { createRealmContext } from '@realm/react';
 import { Chat } from '../realm-database/schemas/Chat';
 import { Message } from '../realm-database/schemas/Message';
+import { Contact } from '../realm-database/schemas/Contact';
 
-export const { RealmProvider, useRealm, useQuery } = createRealmContext({
-  schema: [Chat, Message],
+
+export const realmConfig: Realm.Configuration = {
+  schema: [Chat, Message, Contact],
   schemaVersion: 1,
   deleteRealmIfMigrationNeeded: true,
-});
+};
+
+export const { RealmProvider, useRealm, useQuery } = createRealmContext(realmConfig);
 
 
