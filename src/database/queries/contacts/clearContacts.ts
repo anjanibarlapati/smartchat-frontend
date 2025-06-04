02038b5a052898ff14ecd80d73db9cf359868471
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-04 20:09:53 +0530

    Delete files related to sqlite db

--- src/database/queries/contacts/clearContacts.ts
+++
@@ -1,9 +0,0 @@
-import SQLite from 'react-native-sqlite-storage';
-
-export const clearContacts = async (db: SQLite.SQLiteDatabase) => {
-    try{
-        await db.executeSql('DELETE FROM contacts');
-    } catch(error){
-        throw new Error('Failed to clear contacts in local DB');
-    }
-};
