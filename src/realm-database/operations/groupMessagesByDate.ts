6d705206f741f448f69cc57f5f2fdbb789d13c5a
Anjali Gogu <anjali.gogu@everest.engineering>
2025-06-04 10:38:01 +0530

    Change the function name

---
+++ src/realm-database/operations/groupMessagesByDate.ts
@@ -0,0 +1,11 @@
+import {format, isToday, isYesterday} from 'date-fns';
+import {Message} from '../schemas/Message';
+
+export const groupMessagesByDate = (messages: Realm.Results<Message>) => {
+  const grouped: {[key: string]: Message[]} = {};
+
+  
+  return grouped;
+};
+
+
