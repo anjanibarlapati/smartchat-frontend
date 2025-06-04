7e18d7c957e178cc9c2860d811d51b9f22ac5ac6
SoumyaKatta <soumya.katta@everest.engineering>
2025-06-04 14:36:30 +0530

    Initialize Set to collect unique message dates

--- src/realm-database/operations/groupMessagesByDate.ts
+++ src/realm-database/operations/groupMessagesByDate.ts
@@ -2,6 +2,7 @@ import {Message} from '../schemas/Message';
 
 export const groupMessagesByDate = (messages: Realm.Results<Message>) => {
   const groupedMessages: {[date: string]: Message[]} = {};
+  const dates = new Set<string>();
 
   return grouped;
 };
