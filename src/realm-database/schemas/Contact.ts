c6f221349369249e3487abd66a694c665d592423
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-04 20:04:22 +0530

    Add contacts schema

---
+++ src/realm-database/schemas/Contact.ts
@@ -0,0 +1,23 @@
+import Realm from 'realm';
+
+export class Contact extends Realm.Object<Contact> {
+  _id!: Realm.BSON.ObjectId;
+  name!: string;
+  originalNumber!: string;
+  mobileNumber!: string;
+  profilePicture!: string;
+  doesHaveAccount!: boolean;
+
+  static schema: Realm.ObjectSchema = {
+    name: 'Contact',
+    primaryKey: '_id',
+    properties: {
+      _id: 'objectId',
+      name: 'string',
+      originalNumber: 'string',
+      mobileNumber: 'string',
+      profilePicture: 'string',
+      doesHaveAccount: 'bool',
+    },
+  };
+}
