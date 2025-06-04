636481dfcdc953348df1a3193219aec5d3665d6c
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-06-04 14:31:48 +0530

    Initialize groupedMessages object

--- src/realm-database/operations/groupMessagesByDate.ts
+++ src/realm-database/operations/groupMessagesByDate.ts
@@ -1,8 +1,9 @@
 import {Message} from '../schemas/Message';
 
 export const groupMessagesByDate = (messages: Realm.Results<Message>) => {
-  const grouped: {[key: string]: Message[]} = {};
-
+  const groupedMessages: {[date: string]: Message[]} = {};
 
   return grouped;
 };
+
+
