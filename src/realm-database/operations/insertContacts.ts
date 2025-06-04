c146f736bc3c33a41839898c29fc435375013e63
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-04 20:08:51 +0530

    Add function to insert contacts into realm db

---
+++ src/realm-database/operations/insertContacts.ts
@@ -0,0 +1,20 @@
+import Realm, { BSON } from 'realm';
+import { Contact } from '../../types/Contacts';
+
+export const insertContactsInRealm = (realm: Realm, contacts: Contact[] ) => {
+  realm.write(() => {
+    contacts.forEach((contact) => {
+        realm.create(
+          'Contact',
+          {
+            _id: new BSON.ObjectId(),
+            name: contact.name,
+            originalNumber: contact.originalNumber,
+            mobileNumber: contact.mobileNumber,
+            doesHaveAccount: !!contact.doesHaveAccount,
+            profilePicture: contact.profilePicture ? contact.profilePicture : '',
+          },
+        );
+      });
+  });
+};
