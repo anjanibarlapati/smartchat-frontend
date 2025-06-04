372e5366854d0bfc9e7d2c6105584a79f345dfaf
Shailaja Nimmagari <shailaja.nimmagari@everest.engineering>
2025-06-04 14:41:43 +0530

    Implement grouping of messages by sentAt date

--- src/realm-database/operations/groupMessagesByDate.ts
+++ src/realm-database/operations/groupMessagesByDate.ts
@@ -1,10 +1,17 @@
-import {Message} from '../schemas/Message';
+import { Message } from '../../types/message';
+import { Message as MessageSchema } from '../schemas/Message';
 
-export const groupMessagesByDate = (messages: Realm.Results<Message>) => {
+export const groupMessagesByDate = (messages: Realm.Results<MessageSchema>) => {
   const groupedMessages: {[date: string]: Message[]} = {};
   const dates = new Set<string>();
-
-  return grouped;
+  for (let i = 0; i < messages.length; i++) {
+    const msg = messages[i];
+    const date = msg.sentAt;
+    dates.add(date);
+    if (!groupedMessages[date]) {
+      groupedMessages[date] = [];
+    }
+    groupedMessages[date].push(msg);
+  }
+  return groupedMessages;
 };
-
-
