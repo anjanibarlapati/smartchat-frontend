39511033a323cd15beab10b406309b5a6bd3491b
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-05 12:23:21 +0530

    Add test cases for fetching chats from realm in home screen

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -8,6 +8,11 @@ import { authReducer } from '../../redux/reducers/auth.reducer';
 import { themeReducer } from '../../redux/reducers/theme.reducer';
 import { store } from '../../redux/store';
 import { Home } from './Home';
+import { useHomeChats } from '../../hooks/homechats';
+
+jest.mock('../../hooks/homechats', () => ({
+  useHomeChats: jest.fn(),
+}));
 
 function renderHomeScreen() {
   return render(
@@ -26,6 +31,14 @@ jest.mock('react-native-encrypted-storage', () => ({
   clear: jest.fn(),
 }));
 
+jest.mock('@realm/react', () => {
+  return {
+    useRealm: jest.fn(() => ({})),
+    useQuery: jest.fn(() => []),
+    RealmProvider: ({ children }: any) => <>{children}</>,
+  };
+});
+
 describe('Home Screen', () => {
   const mockNavigate = jest.fn();
   const showAlertMock = jest.fn();
@@ -35,7 +48,7 @@ describe('Home Screen', () => {
       navigate: mockNavigate,
       reset: jest.fn(),
     });
-
+    (useHomeChats as jest.Mock).mockReturnValue([]);
     jest.spyOn(useAlertModalHook, 'useAlertModal').mockReturnValue({
       alertMessage: 'Test success message',
       alertType: 'success',
@@ -90,13 +103,54 @@ describe('Home Screen', () => {
   });
   test('Should apply styles based on the width of the screen', () => {
     const {getByLabelText} = renderHomeScreen();
-    const homeImage = getByLabelText('home-image').parent?.parent;
-    expect(homeImage?.props.style.paddingTop).toBe('15%');
+    const homeImage = getByLabelText('home-image');
+    expect(homeImage.props.style.height).toBe(300);
     jest
       .spyOn(require('react-native'), 'useWindowDimensions')
       .mockReturnValue({width: 10, height: 100});
     renderHomeScreen();
-    const homeImg = screen.getByLabelText('home-image').parent?.parent?.parent;
-    expect(homeImg?.props.style.paddingTop).toBe('40%');
+    const homeImg = screen.getByLabelText('home-image');
+    expect(homeImg.props.style.height).toBe(250);
+  });
+
+  test('should render unread chat list if available', () => {
+    const mockUnreadChats = [
+      {
+        lastMessage: {
+          message: 'Hello Anjani',
+          sentAt: '2025-06-03T10:00:00Z',
+          isSender: false,
+          status: 'delivered',
+        },
+        unreadCount: 3,
+        contact: {
+          name: 'Anjaniii',
+          originalNumber: '8639523822',
+          mobileNumber: '+91 86395 23822',
+          profilePicture: null,
+        },
+      },
+      {
+        lastMessage: {
+          message: 'Anjuu',
+          sentAt: '2025-06-04T10:00:00Z',
+          isSender: false,
+          status: 'delivered',
+        },
+        unreadCount: 0,
+        contact: {
+          name: 'Anjani',
+          originalNumber: '8639523823',
+          mobileNumber: '+91 86395 23823',
+          profilePicture: null,
+        },
+      },
+    ];
+    (useHomeChats as jest.Mock).mockReturnValue(mockUnreadChats);
+    const { getByText } = renderHomeScreen();
+    expect(getByText('Anjaniii')).toBeTruthy();
+    expect(getByText('Hello Anjani')).toBeTruthy();
+    expect(getByText('Anjuu')).toBeTruthy();
+    expect(getByText('Anjani')).toBeTruthy();
   });
 });
