d5e90ac02840d4045ec86ca48ba8bab9787ce073
MamathaNiyal23 <mamatha.niyal@everest.engineering>
2025-06-05 22:11:27 +0530

    Store and fetch pending messages and make api call to backend to store unsend messages in db, update tests accordingly

---
+++ src/utils/storePendingMessages.ts
@@ -0,0 +1,51 @@
+import Realm from 'realm';
+import {Message} from '../realm-database/schemas/Message';
+import {encryptMessage} from './encryptMessage';
+import {getTokens} from './getTokens';
+import {unsendMessages} from './unsendMessages';
+
+export type MessageToSend = {
+  receiverMobileNumber: string;
+  message: string;
+  nonce: string;
+  sentAt: string;
+};
+
+export const storePendingMessages = async (
+  senderMobileNumber: string,
+  realm: Realm,
+) => {
+  const pendingMessages = realm
+    .objects<Message>('Message')
+    .filtered("status == 'pending' AND isSender == true ");
+
+  if (pendingMessages.length === 0) {
+    return;
+  }
+  const messagesToSend: MessageToSend[] = [];
+  const tokens = await getTokens(senderMobileNumber);
+  for (const message of pendingMessages) {
+    if (message.chat) {
+      const {ciphertext, nonce} = await encryptMessage(
+        message.chat.chatId,
+        message.message,
+        tokens.access_token,
+      );
+      messagesToSend.push({
+        receiverMobileNumber: message.chat.chatId,
+        message: ciphertext,
+        nonce,
+        sentAt: message.sentAt,
+      });
+    }
+  }
+  const response = await unsendMessages(senderMobileNumber, messagesToSend);
+
+  if (response.ok) {
+    realm.write(() => {
+      pendingMessages.forEach(msg => {
+        msg.status = 'sent';
+      });
+    });
+  }
+};
