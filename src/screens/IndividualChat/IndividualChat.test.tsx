df4efd3cc036e931b36fcc65aa51bc3e1019c1ac
Anjali Gogu <anjali.gogu@everest.engineering>
2025-06-05 21:56:45 +0530

    Update mock return value to include sorted

--- src/screens/IndividualChat/IndividualChat.test.tsx
+++ src/screens/IndividualChat/IndividualChat.test.tsx
@@ -1,39 +1,36 @@
-import { Provider } from 'react-redux';
-import { NavigationContainer, useNavigation, useRoute} from '@react-navigation/native';
-import { configureStore } from '@reduxjs/toolkit';
-import { render, screen, waitFor } from '@testing-library/react-native';
-import { useQuery, useRealm } from '../../contexts/RealmContext';
-import { updateMessageStatusInRealm } from '../../realm-database/operations/updateMessageStatus';
-import { themeReducer } from '../../redux/reducers/theme.reducer';
-import { userReducer } from '../../redux/reducers/user.reducer';
-import { getSocket } from '../../utils/socket';
-import { IndividualChat } from './IndividualChat';
-
+import {Provider} from 'react-redux';
+import {
+  NavigationContainer,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import {configureStore} from '@reduxjs/toolkit';
+import {render, screen, waitFor} from '@testing-library/react-native';
+import {useQuery, useRealm} from '../../contexts/RealmContext';
+import {updateMessageStatusInRealm} from '../../realm-database/operations/updateMessageStatus';
+import {themeReducer} from '../../redux/reducers/theme.reducer';
+import {userReducer} from '../../redux/reducers/user.reducer';
+import {getSocket} from '../../utils/socket';
+import {IndividualChat} from './IndividualChat';
 jest.mock('react-native-encrypted-storage', () => ({
   getItem: jest.fn(),
   setItem: jest.fn(),
 }));
-
 jest.mock('react-native-localize', () => ({
   getCountry: jest.fn(() => 'IN'),
 }));
-
-
 jest.mock('../../realm-database/operations/updateMessageStatus', () => ({
   updateMessageStatusInRealm: jest.fn(),
 }));
-
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: jest.fn(),
   useRoute: jest.fn(),
 }));
-
 jest.mock('../../contexts/RealmContext', () => ({
   useQuery: jest.fn(),
   useRealm: jest.fn(),
 }));
-
 let mockEmit: jest.Mock = jest.fn();
 jest.mock('../../utils/socket', () => ({
   getSocket: jest.fn(() => ({
@@ -41,28 +38,24 @@ jest.mock('../../utils/socket', () => ({
     emit: mockEmit,
   })),
 }));
-
 jest.mock('realm', () => ({
   BSON: {
     ObjectId: jest.fn(() => 'mocked-object-id'),
   },
+  Object: class {},
 }));
-
 jest.mock('react-native-libsodium', () => ({
   crypto_box_keypair: jest.fn(),
   to_base64: jest.fn(),
 }));
-
 jest.mock('../../components/ChatOptionsModal/blockChat.service', () => ({
-  blockUserChat: jest.fn(() => Promise.resolve({ ok: true })),
+  blockUserChat: jest.fn(() => Promise.resolve({ok: true})),
 }));
-
 jest.mock('../../realm-database/operations/blockContact', () => ({
   blockContactInRealm: jest.fn(),
 }));
-
 const mockRealm = {
-  write: jest.fn((fn) => fn()),
+  write: jest.fn(fn => fn()),
   objectForPrimaryKey: jest.fn().mockReturnValue({
     chatId: '+91 86395 23822',
     isBlocked: false,
@@ -70,12 +63,9 @@ const mockRealm = {
   }),
   create: jest.fn(),
 };
-
-
 describe('IndividualChat', () => {
   let store: ReturnType<typeof configureStore>;
   const sentAt = new Date().toISOString();
-
   const unseenMessage = {
     _id: '1',
     message: 'Unread message',
@@ -86,10 +76,9 @@ describe('IndividualChat', () => {
       chatId: '+91 86395 23822',
     },
   };
-
   const seenMessage = {
     _id: '2',
-    message: 'Hello Anjani',
+    message: 'Hello Anji',
     sentAt,
     isSender: true,
     status: 'seen',
@@ -97,28 +86,26 @@ describe('IndividualChat', () => {
       chatId: '+91 86395 23833',
     },
   };
-
   const renderComponent = () =>
     render(
       <NavigationContainer>
         <Provider store={store}>
           <IndividualChat />
         </Provider>
-      </NavigationContainer>
+      </NavigationContainer>,
     );
-
   beforeEach(() => {
     jest.clearAllMocks();
     (useRealm as jest.Mock).mockReturnValue(mockRealm);
     const setOptionsMock = jest.fn();
-    const getParentMock = jest.fn().mockReturnValue({ setOptions: setOptionsMock });
-
+    const getParentMock = jest
+      .fn()
+      .mockReturnValue({setOptions: setOptionsMock});
     (useNavigation as jest.Mock).mockReturnValue({
       goBack: jest.fn(),
       setOptions: setOptionsMock,
       getParent: getParentMock,
     });
-
     (useRoute as jest.Mock).mockReturnValue({
       params: {
         name: 'Shailu',
@@ -127,7 +114,6 @@ describe('IndividualChat', () => {
         profilePic: 'pic-url',
       },
     });
-
     store = configureStore({
       reducer: {
         theme: themeReducer,
@@ -135,87 +121,82 @@ describe('IndividualChat', () => {
       },
     });
   });
-
-  test('renders messages from the FlatList', () => {
+  test('should render messages from the FlatList', () => {
     (useQuery as jest.Mock).mockReturnValue({
-      filtered: jest.fn().mockReturnValue([seenMessage, unseenMessage]),
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([seenMessage, unseenMessage]),
+      }),
     });
     renderComponent();
-
-    expect(screen.getByText('Hello Anjani')).toBeTruthy();
+    expect(screen.getByText('Hello Anji')).toBeTruthy();
     expect(screen.getByText('Unread message')).toBeTruthy();
   });
-  test('does not render messages when there are no messages', () => {
+  test('should not render messages when there are no messages', () => {
     (useQuery as jest.Mock).mockReturnValue({
-      filtered: jest.fn().mockReturnValue([]),
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([]),
+      }),
     });
-    const { queryByText } = renderComponent();
-
-    expect(queryByText('Hello Anjani')).toBeNull();
+    const {queryByText} = renderComponent();
+    expect(queryByText('Hello Anji')).toBeNull();
   });
-
-
-  test('renders the InputChatBox', () => {
+  test('should render the InputChatBox', () => {
     (useQuery as jest.Mock).mockReturnValue({
-      filtered: jest.fn().mockReturnValue([seenMessage]),
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([seenMessage]),
+      }),
     });
-
     renderComponent();
-
     const input = screen.getByPlaceholderText('Type a message');
     expect(input).toBeTruthy();
   });
-
-  test('renders the menu icon (send icon)', () => {
-  (useQuery as jest.Mock).mockReturnValue({
-    filtered: jest.fn().mockReturnValue([seenMessage]),
-   });
-
+  test('should render the menu icon (send icon)', () => {
+    (useQuery as jest.Mock).mockReturnValue({
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([seenMessage]),
+      }),
+    });
     renderComponent();
-
     const icon = screen.getByLabelText('send-icon');
     expect(icon).toBeTruthy();
   });
-
-  test('calls navigation.setOptions twice (header + tabBar)', () => {
-  (useQuery as jest.Mock).mockReturnValue({
-    filtered: jest.fn().mockReturnValue([seenMessage]),
-   });
-
+  test('should call navigation.setOptions twice', () => {
+    (useQuery as jest.Mock).mockReturnValue({
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([seenMessage]),
+      }),
+    });
     renderComponent();
-
     const navigation = useNavigation();
     expect(navigation.setOptions).toHaveBeenCalledTimes(2);
   });
-
   test('should not emit messageRead if socket is not connected', async () => {
     (getSocket as jest.Mock).mockReturnValue({
       connected: false,
       emit: mockEmit,
     });
     (useQuery as jest.Mock).mockReturnValue({
-      filtered: jest.fn().mockReturnValue([unseenMessage]),
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([unseenMessage]),
+      }),
     });
-
     renderComponent();
-
     await waitFor(() => {
       expect(mockEmit).not.toHaveBeenCalled();
     });
   });
-
   test('should emit messageRead for unread received messages', async () => {
     (getSocket as jest.Mock).mockReturnValue({
       connected: true,
       emit: mockEmit,
     });
-  (useQuery as jest.Mock).mockReturnValue({
-    filtered: jest.fn().mockReturnValue([unseenMessage, seenMessage]),
-   });
-  (updateMessageStatusInRealm as jest.Mock).mockReturnValue({});
-
+    (useQuery as jest.Mock).mockReturnValue({
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([seenMessage, unseenMessage]),
+      }),
+    });
+    (updateMessageStatusInRealm as jest.Mock).mockReturnValue({});
     renderComponent();
-
     await waitFor(() => {
       expect(mockEmit).toHaveBeenCalledWith('messageRead', {
         sentAt,
@@ -223,8 +204,7 @@ describe('IndividualChat', () => {
       });
     });
   });
-
-  test('Should creates a new Chat if chat does not exist in Realm', () => {
+  test('should create a new Chat if chat does not exist in Realm', () => {
     mockRealm.objectForPrimaryKey.mockReturnValue(undefined);
     renderComponent();
     expect(mockRealm.write).toHaveBeenCalled();
@@ -234,20 +214,19 @@ describe('IndividualChat', () => {
       publicKey: null,
     });
   });
-  test('renders blocked message box when chat is blocked', () => {
+  test('should render blocked message box when chat is blocked', () => {
     mockRealm.objectForPrimaryKey.mockReturnValue({
       chatId: '+91 86395 23822',
       isBlocked: true,
       publicKey: null,
     });
-
     (useQuery as jest.Mock).mockReturnValue({
-      filtered: jest.fn().mockReturnValue([]),
+      filtered: jest.fn().mockReturnValue({
+        sorted: jest.fn().mockReturnValue([]),
+      }),
     });
-
-    const { getByText } = renderComponent();
-
+    const {getByText} = renderComponent();
     expect(getByText(/You have blocked this contact/i)).toBeTruthy();
   });
-
 });
+
