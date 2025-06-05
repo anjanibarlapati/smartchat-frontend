dca1efd2bbd6605e4c7304e927adcdd4052c8f97
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-05 12:25:11 +0530

    Add test cases for fetching unread chats from realm in unread screen

--- src/screens/Unread/Unread.test.tsx
+++ src/screens/Unread/Unread.test.tsx
@@ -4,10 +4,15 @@ import {store} from '../../redux/store';
 import {useNavigation} from '@react-navigation/native';
 import {fireEvent, render} from '@testing-library/react-native';
 import {Unread} from './Unread';
+import { useUnreadChats } from '../../hooks/unreadChats';
 
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
   useNavigation: jest.fn(),
+}));
+
+jest.mock('../../hooks/unreadChats', () => ({
+  useUnreadChats: jest.fn(),
 }));
 
 function renderUnreadScreen() {
@@ -30,35 +35,66 @@ describe('Unread Screen', () => {
   });
 
   test('should render "No unread chats here" text', () => {
+    (useUnreadChats as jest.Mock).mockReturnValue([]);
     const {getByText} = renderUnreadScreen();
     expect(getByText(/No unread chats here/i)).toBeTruthy();
   });
 
   test('should render "View all chats" text', () => {
+    (useUnreadChats as jest.Mock).mockReturnValue([]);
     const {getByText} = renderUnreadScreen();
     expect(getByText(/View all chats/i)).toBeTruthy();
   });
 
   test('should navigate to AllChatsTab when pressing "View all chats"', () => {
+    (useUnreadChats as jest.Mock).mockReturnValue([]);
     const {getByText} = renderUnreadScreen();
     fireEvent.press(getByText(/View all chats/i));
     expect(mockNavigateToAllChats).toHaveBeenCalledWith('AllChatsTab');
   });
 
   test('should render add contact button', () => {
+    (useUnreadChats as jest.Mock).mockReturnValue([]);
     const {getByLabelText} = renderUnreadScreen();
     expect(getByLabelText('addContact')).toBeTruthy();
   });
 
   test('should navigate to Contact when pressing add contact button', () => {
+    (useUnreadChats as jest.Mock).mockReturnValue([]);
     const {getByLabelText} = renderUnreadScreen();
     fireEvent.press(getByLabelText('addContact'));
     expect(mockNavigateToContact).toHaveBeenCalledWith('Contact');
   });
 
-test('Should render the home image properly', ()=>{
+  test('Should render the home image properly', ()=>{
+    (useUnreadChats as jest.Mock).mockReturnValue([]);
     const {getByLabelText} = renderUnreadScreen();
     expect(getByLabelText('unread-image')).toBeTruthy();
+  });
+  test('should render unread chat list if available', () => {
+    const mockUnreadChats = [
+      {
+        lastMessage: {
+          message: 'Hello Anjani',
+          sentAt: '2025-06-03T10:00:00Z',
+          isSender: false,
+          status: 'delivered',
+        },
+        unreadCount: 3,
+        contact: {
+          name: 'Anjaniii',
+          originalNumber: '8639523822',
+          mobileNumber: '+91 86395 23822',
+          profilePicture: null,
+        },
+      },
+    ];
+
+    (useUnreadChats as jest.Mock).mockReturnValue(mockUnreadChats);
+    const { getByText } = renderUnreadScreen();
+
+    expect(getByText('Anjaniii')).toBeTruthy();
+    expect(getByText('Hello Anjani')).toBeTruthy();
   });
 
 });
