fdae09291e0fd0fc629b571c14de8f2ed81ea6be
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-05 12:26:04 +0530

    Fetch unread chats from custom hook and render using flat list

--- src/screens/Unread/Unread.tsx
+++ src/screens/Unread/Unread.tsx
@@ -1,38 +1,55 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Image, ScrollView, Text, TouchableOpacity, useWindowDimensions, View } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, useWindowDimensions, View } from 'react-native';
 import { useAppTheme } from '../../hooks/appTheme';
 import { AllChatsTabScreenNavigationProps, ContactScreenNavigationProps } from '../../types/Navigations';
 import { Theme } from '../../utils/themes';
 import { getStyles } from '../Home/Home.styles';
-import { getStyles as UnreadStyles } from './Unread.styles';
+import { ChatCard } from '../../components/ChatCard/ChatCard';
+import { useUnreadChats } from '../../hooks/unreadChats';
+import { HomeChats } from '../../hooks/homechats';
 
 export function Unread(): React.JSX.Element {
 
   const {width} = useWindowDimensions();
   const theme: Theme = useAppTheme();
   const styles = getStyles(theme, width);
-  const Ustyles = UnreadStyles(theme);
   const navigation = useNavigation<ContactScreenNavigationProps>();
   const navigationToAllChats = useNavigation<AllChatsTabScreenNavigationProps>();
+  const unreadChats: HomeChats[] = useUnreadChats();
   return (
     <View style={styles.container}>
       <View style={styles.totalContainer}>
-        <ScrollView contentContainerStyle={styles.bodyContainer}>
-          <View style={styles.textContainer}>
-             <Image
-                               source={theme.images.unreadImageIcon}
-                                style={styles.homeImageStyles}
-                                accessibilityLabel="unread-image"
-                              />
-            <Text style={Ustyles.bodyText}>
-              No unread chats here
-            </Text>
-            <TouchableOpacity onPress={() => navigationToAllChats.navigate('AllChatsTab')}>
-            <Text style={Ustyles.viewText}>View all chats</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+          {unreadChats.length === 0 ? (
+            <View style={styles.unreadTextContainer}>
+
+                <Image
+                  source={theme.images.unreadImageIcon}
+                  style={styles.homeImageStyles}
+                  accessibilityLabel="unread-image"
+                />
+              <Text style={styles.unReadBodyText}>
+                No unread chats here
+              </Text>
+              <TouchableOpacity onPress={() => navigationToAllChats.navigate('AllChatsTab')}>
+                <Text style={styles.viewText}>View all chats</Text>
+              </TouchableOpacity>
+            </View>
+           ) : (
+              <View style={styles.homeChatsContainer}>
+                <FlatList
+                    data={unreadChats}
+                    keyExtractor={(item) => item.contact.mobileNumber}
+                    renderItem={({ item }) => (
+                    <ChatCard
+                      contact={item.contact}
+                      message={item.lastMessage}
+                      unreadCount={item.unreadCount}
+                    />
+                    )}
+                  />
+                </View>
+        )}
         <TouchableOpacity
           onPress={() => navigation.navigate('Contact')}
           style={styles.addContactContainer}>
