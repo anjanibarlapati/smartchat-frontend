f76955860340a24784738d38276d181d6e069c50
SoumyaKatta <soumya.katta@everest.engineering>
2025-06-09 15:00:37 +0530

    Add test case to returns nothing when there are no unread messages

---
+++ src/hooks/unreadChatsCount.test.ts
@@ -0,0 +1,52 @@
+import { renderHook } from '@testing-library/react-native';
+import { useUnreadChatsCount } from './unreadChatsCount';
+import { useQuery } from '../contexts/RealmContext';
+import { Message } from '../realm-database/schemas/Message';
+
+jest.mock('../contexts/RealmContext', () => ({
+  useQuery: jest.fn(),
+}));
+
+const mockUseQuery = useQuery as jest.Mock;
+
+type RealmCollection<T> = T[] & {
+  filtered: (query: string) => RealmCollection<T>;
+};
+
+function createRealmCollection<T extends Record<string, any>>(items: T[]): RealmCollection<T> {
+  const collection = [...items] as RealmCollection<T>;
+
+  collection.filtered = ((query: string): RealmCollection<T> => {
+    if (query === 'isSender == false AND status != "seen"') {
+      return createRealmCollection(
+        collection.filter((m:T) => !m.isSender && m.status !== 'seen')
+      );
+    }
+    return createRealmCollection([]);
+  }) as RealmCollection<T>['filtered'];
+
+  return collection;
+}
+describe('shoud render useUnreadChatsCount', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should returns 0 when there are no unread messages', () => {
+    const messages: Message[] = [];
+    mockUseQuery.mockReturnValue(createRealmCollection(messages));
+
+    const { result } = renderHook(() => useUnreadChatsCount());
+    expect(result.current).toBe(0);
+  });
+
+  it('should return correct number of unique chat IDs', () => {
+    const messages = [
+      { chat: { chatId: '1' }, isSender: false, status: 'delivered' },
+      { chat: { chatId: '2' }, isSender: false, status: 'delivered' },
+      { chat: { chatId: '1' }, isSender: false, status: 'delivered' },
+    ];
+    mockUseQuery.mockReturnValue(createRealmCollection(messages));
+
+    const { result } = renderHook(() => useUnreadChatsCount());
+    expect(result.current).toBe(2);
+  });
+});
