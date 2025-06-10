f44956eb2261957c1034ffb93b64aa61780a3174
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-06-10 15:08:24 +0530

    Create object with priority of message status

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,11 @@
 import { Platform } from 'react-native';
+import { MessageStatus } from '../types/message';
 
 export const BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:5050/' : 'http://localhost:5050/';
+
+export const STATUS_ORDER: Record<MessageStatus, number> = {
+  pending: 0,
+  sent: 1,
+  delivered: 2,
+  seen: 3,
+};
