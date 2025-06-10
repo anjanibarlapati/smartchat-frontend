6815f411dc9a8fdf7c66a4561efff53c9dbfb4f1
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-10 10:22:18 +0530

    Create a custom context for resetting the realm

--- src/contexts/RealmContext.tsx
+++ src/contexts/RealmContext.tsx
@@ -1,7 +1,10 @@
 import { createRealmContext } from '@realm/react';
+import { createContext, ReactNode, useContext, useState } from 'react';
+import Realm from 'realm';
+import { deleteAllRealmData } from '../realm-database/connection';
 import { Chat } from '../realm-database/schemas/Chat';
-import { Message } from '../realm-database/schemas/Message';
 import { Contact } from '../realm-database/schemas/Contact';
+import { Message } from '../realm-database/schemas/Message';
 
 
 export const realmConfig: Realm.Configuration = {
@@ -10,6 +13,27 @@ export const realmConfig: Realm.Configuration = {
   deleteRealmIfMigrationNeeded: true,
 };
 
-export const { RealmProvider, useRealm, useQuery } = createRealmContext(realmConfig);
+const { RealmProvider, useRealm, useQuery } = createRealmContext(realmConfig);
 
+const RealmResetContext = createContext({ resetRealm: () => {} });
+
+export function RealmResetProvider({ children }: { children: ReactNode }): React.JSX.Element {
+
+    const [realmKey, setRealmKey] = useState<number>(0);
+    const resetRealm = () => {
+      deleteAllRealmData();
+      setRealmKey(previousKey => previousKey + 1);
+    };
+
+    return (
+      <RealmResetContext.Provider value={{ resetRealm }}>
+        <RealmProvider key={realmKey}>
+          {children}
+        </RealmProvider>
+      </RealmResetContext.Provider>
+    );
+}
+
+export const useRealmReset = () => useContext(RealmResetContext);
+export { useQuery, useRealm };
 
