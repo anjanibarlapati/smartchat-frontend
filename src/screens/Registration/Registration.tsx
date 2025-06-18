ebf6013b83402f928380c80f4877a852347a47fc
Anjali Gogu <anjali.gogu@everest.engineering>
2025-06-18 12:37:04 +0530

    Remove KeyboardAvoidingView in registration screen

--- src/screens/Registration/Registration.tsx
+++ src/screens/Registration/Registration.tsx
@@ -3,8 +3,8 @@ import {parsePhoneNumberFromString} from 'libphonenumber-js';
 import React, {useState} from 'react';
 import {
   Image,
-  KeyboardAvoidingView,
-  Platform,
+  // KeyboardAvoidingView,
+  // Platform,
   ScrollView,
   Text,
   TouchableOpacity,
@@ -68,7 +68,7 @@ const Registration = () => {
   });
 
   const handleChange = (field: string, value: string) => {
-    if(field === 'email') {
+    if (field === 'email') {
       value = value.trim().toLowerCase();
     }
     setUser(prevValues => ({
@@ -243,12 +243,9 @@ const Registration = () => {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 50 : 0}
-      style={styles.container}>
+    <View style={styles.container}>
       <ScrollView
-        contentContainerStyle={styles.body}
+        contentContainerStyle={[styles.body, styles.scrollViewContent]}
         accessibilityLabel="body-container"
         keyboardShouldPersistTaps="handled">
         <TouchableOpacity
@@ -357,7 +354,7 @@ const Registration = () => {
         type={alertType}
         onClose={hideAlert}
       />
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
