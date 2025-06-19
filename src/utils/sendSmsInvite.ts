04a1671060491c706ef7245281beb8fc258a6fcd
Anjali Gogu <anjali.gogu@everest.engineering>
2025-06-20 00:43:15 +0530

    Handle Android 34+ SMS permission

--- src/utils/sendSmsInvite.ts
+++ src/utils/sendSmsInvite.ts
@@ -1,40 +1,60 @@
-import { Alert, Linking, Platform } from 'react-native';
-import { requestPermission } from '../permissions/permissions';
+import {Alert, Linking, Platform} from 'react-native';
+import {requestPermission} from '../permissions/permissions';
 import SendSMS from 'react-native-sms';
 
 export const requestSmsPermission = async () => {
-    const isPermissionGranted = await requestPermission('send-sms');
-    if (!isPermissionGranted) {
-        return false;
-    }
-    return true;
+  if (Platform.OS === 'android' && Number(Platform.Version) >= 34) {
+    return false;
+  }
+  const isPermissionGranted = await requestPermission('send-sms');
+  if (!isPermissionGranted) {
+    return false;
+  }
+  return true;
 };
 
 export const sendSmsInvite = async (mobileNumber: string) => {
-    const inviteMessage = 'Let\'s chat on SmartChat! It\'s a fast, simple, and secure app we can use to message each other for free.';
-    const granted = await requestSmsPermission();
-    if (!granted) {
-        Alert.alert('Permission denied', 'SMS permission is required to send invites.');
-        return;
-    }
-    if(Platform.OS === 'ios'){
-         const url = `sms:${mobileNumber}&body=${encodeURIComponent(inviteMessage)}`;
-         Linking.openURL(url).catch(err => console.error('SMS error:', err));
-         return;
-    }
-    SendSMS.send({
-        body: inviteMessage,
-        recipients: [mobileNumber],
-        successTypes: ['sent', 'queued'] as any,
-        allowAndroidSendWithoutReadPermission: true,
-    }, (completed, cancelled, error) => {
-        if (completed) {
-            console.log('SMS Sent Successfully');
-        } else if (cancelled) {
-            console.log('SMS Sending Cancelled');
-        } else if (error) {
-            console.log('Failed to send SMS');
-        }
-    });
+  const inviteMessage =
+    "Let's chat on SmartChat! It's a fast, simple, and secure app we can use to message each other for free.";
+  if (Platform.OS === 'ios') {
+    const url = `sms:${mobileNumber}&body=${encodeURIComponent(inviteMessage)}`;
+    Linking.openURL(url).catch(err => console.error('SMS error:', err));
     return;
+  }
+  if (Platform.OS === 'android') {
+    if (Number(Platform.Version) >= 34) {
+      const url = `sms:${mobileNumber}?body=${encodeURIComponent(
+        inviteMessage,
+      )}`;
+      Linking.openURL(url).catch(err => console.error('SMS error:', err));
+      return;
+    } else {
+      const granted = await requestSmsPermission();
+      if (!granted) {
+        Alert.alert(
+          'Permission denied',
+          'SMS permission is required to send invites.',
+        );
+        return;
+      }
+
+      SendSMS.send(
+        {
+          body: inviteMessage,
+          recipients: [mobileNumber],
+          successTypes: ['sent', 'queued'] as any,
+          allowAndroidSendWithoutReadPermission: true,
+        },
+        (completed, cancelled, error) => {
+          if (completed) {
+            console.log('SMS Sent Successfully');
+          } else if (cancelled) {
+            console.log('SMS Sending Cancelled');
+          } else if (error) {
+            console.log('Failed to send SMS');
+          }
+        },
+      );
+    }
+  }
 };
