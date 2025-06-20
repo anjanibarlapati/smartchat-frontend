3021d5440f79c9faa983b9f45ad6e7cc00650e48
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-20 05:53:41 +0530

    Add custom alert for force logout

--- src/screens/Welcome/Welcome.tsx
+++ src/screens/Welcome/Welcome.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { Image, Pressable, Text, useWindowDimensions, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { Image, Pressable, Text, useWindowDimensions, View } from 'react-native';
+import {  useSelector } from 'react-redux';
+import { CustomAlert } from '../../components/CustomAlert/CustomAlert';
 import { useAppTheme } from '../../hooks/appTheme';
+import { useAlertModal } from '../../hooks/useAlertModal';
+import { storeState } from '../../redux/store';
 import { WelcomeScreenNavigationProps } from '../../types/Navigations';
 import { Theme } from '../../utils/themes';
 import { getStyles } from './Welcome.styles';
@@ -13,6 +17,14 @@ const WelcomeScreen = ():React.JSX.Element => {
 
   const theme: Theme = useAppTheme();
   const styles = getStyles(theme, width, height);
+  const { alertVisible, alertMessage, alertType, showAlert, hideAlert } = useAlertModal();
+  const successMessage = useSelector((state: storeState) => state.auth.successMessage);
+  useEffect(() => {
+    if (successMessage === 'logged-out') {
+      showAlert('You have been logged out due to another device login.', 'info');
+    }
+  }, [ successMessage,  showAlert]);
+
   return (
     <View style={styles.container} >
       <Image
@@ -24,7 +36,7 @@ const WelcomeScreen = ():React.JSX.Element => {
         <Text style={styles.appname}>SmartChat</Text>
         <Text style={styles.apptagline}>Where conversations evolve</Text>
       </View>
-      <Pressable style={styles.getstartButton} onPress={()=>navigation.replace('RegistrationScreen')}>
+      <Pressable style={styles.getstartButton} onPress={() => navigation.replace('RegistrationScreen')}>
         <View style={styles.buttonContent}>
           <Text style={styles.getstartText}>Let's Get Started</Text>
           <Image
@@ -34,6 +46,7 @@ const WelcomeScreen = ():React.JSX.Element => {
           />
         </View>
       </Pressable>
+      <CustomAlert visible={alertVisible} message={alertMessage} type={alertType} onClose={hideAlert} />
     </View>
   );
 };
