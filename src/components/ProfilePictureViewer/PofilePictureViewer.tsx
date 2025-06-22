14aa1922b926ebc12cc16ceb5675f655b3a27bf0
RekhaKorepu17 <rekha.korepu@everest.engineering>
2025-06-22 18:25:24 +0530

    Implement ProfilePictureViewer Modal to view profile

---
+++ src/components/ProfilePictureViewer/PofilePictureViewer.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Image,
+  ImageSourcePropType,
+  Modal,
+  Pressable,
+  Text,
+  TouchableWithoutFeedback,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import {getStyles} from './ProfilePictureViewer.styles';
+
+interface Props {
+  visible: boolean;
+  imageSource: ImageSourcePropType;
+  name?: string;
+  onClose: () => void;
+}
+
+export const ProfilePictureViewerModal: React.FC<Props> = ({
+  visible,
+  imageSource,
+  name,
+  onClose,
+}) => {
+  const {width, height} = useWindowDimensions();
+  const styles = getStyles(width, height);
+
+  return (
+    <Modal visible={visible} transparent animationType="fade">
+      <Pressable
+        style={styles.overlay}
+        onPress={onClose}
+        accessibilityLabel="ProfileModal">
+        <TouchableWithoutFeedback>
+          <View style={styles.imageWrapper}>
+            <Image
+              source={imageSource}
+              style={styles.fullImage}
+              resizeMode="cover"
+            />
+            {name && (
+              <View style={styles.nameContainer}>
+                <Text style={styles.contactName}>{name}</Text>
+              </View>
+            )}
+          </View>
+        </TouchableWithoutFeedback>
+      </Pressable>
+    </Modal>
+  );
+};
