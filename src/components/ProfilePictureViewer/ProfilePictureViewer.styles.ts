0ac4f40a34fd17c8051a8d7e85ebc86cd306d282
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-23 08:08:07 +0530

    Update profile picture styles

--- src/components/ProfilePictureViewer/ProfilePictureViewer.styles.ts
+++ src/components/ProfilePictureViewer/ProfilePictureViewer.styles.ts
@@ -21,6 +21,7 @@ export const getStyles = (width: number, height: number) =>
       width: '100%',
       height: '100%',
       borderRadius: 12,
+      objectFit:'fill',
     },
     nameContainer: {
       position: 'absolute',
