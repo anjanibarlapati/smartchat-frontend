0a2ede1f91507322ef73a04eba806d1f6e9de600
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-23 08:03:20 +0530

    Add profile picture modal when user clicks on contact profile

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -6,6 +6,8 @@ import { ContactScreenNavigationProps } from '../../types/Navigations';
 import { sendSmsInvite } from '../../utils/sendSmsInvite';
 import { Theme } from '../../utils/themes';
 import { getStyles } from './ContactCard.styles';
+import { ProfilePictureViewerModal } from '../ProfilePictureViewer/PofilePictureViewer';
+import { useState } from 'react';
 
 
 function ContactCard ({contact, isSelfContact}: {contact: Contact, isSelfContact: boolean}):React.JSX.Element {
@@ -14,23 +16,31 @@ function ContactCard ({contact, isSelfContact}: {contact: Contact, isSelfContact
     const theme: Theme = useAppTheme();
     const styles = getStyles(theme, width);
     const navigation = useNavigation<ContactScreenNavigationProps>();
+    const [modalVisible, setModalVisible] = useState(false);
+    const profileImageUri = contact.profilePicture ? {uri: contact.profilePicture} : require('../../../assets/images/profileImage.png');
 
     return (
-        <TouchableOpacity style={styles.body} accessibilityLabel="contact-card"
-            onPress={()=> {
+        <View style={styles.body} accessibilityLabel="contact-card">
+            <TouchableOpacity onPress={() => setModalVisible(true)}>
+                <Image
+                    style={styles.profileIcon}
+                    source={profileImageUri}
+                    accessibilityLabel="profile-image"
+                    resizeMode="cover"
+                />
+            </TouchableOpacity>
+            <ProfilePictureViewerModal
+            visible={modalVisible}
+            imageSource={profileImageUri}
+            name={contact.name}
+            onClose={() => setModalVisible(false)}
+            />
+            <TouchableOpacity style={styles.contactContainer} accessibilityLabel="contact-container"
+                onPress={()=> {
                 contact.doesHaveAccount ?
                 navigation.replace('IndividualChat', {name: contact.name, originalNumber: contact.originalNumber, mobileNumber: contact.mobileNumber, profilePic: contact.profilePicture})
-                : sendSmsInvite(contact.originalNumber);}
-            }
-        >
-            <Image
-                style={styles.profileIcon}
-                source={contact.profilePicture ? {uri: contact.profilePicture}
-                : require('../../../assets/images/profileImage.png') }
-                accessibilityLabel="profile-image"
-                resizeMode="cover"
-            />
-            <View style={styles.contactContainer} accessibilityLabel="contact-container">
+                : sendSmsInvite(contact.originalNumber);}}
+            >
                 <View style={styles.nameInviteContainer}>
                     <Text numberOfLines={1} ellipsizeMode="tail" style={styles.contactName}>{`${contact.name}${isSelfContact ? ' (You)' : ''}`}</Text>
                     { !contact.doesHaveAccount && <Pressable onPress= {()=> sendSmsInvite(contact.originalNumber)}>
@@ -38,8 +48,8 @@ function ContactCard ({contact, isSelfContact}: {contact: Contact, isSelfContact
                     </Pressable> }
                 </View>
                 <Text style={styles.contactNumber}>{contact.originalNumber}</Text>
-            </View>
-        </TouchableOpacity>
+            </TouchableOpacity>
+        </View>
     );
 }
 
