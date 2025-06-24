592a3e89decd89b41f1e7f3158b9adc04a6b82a7
Mamatha Niyal <mamathaniyal@Mamathas-MacBook-Pro.local>
2025-06-24 16:37:47 +0530

    Add styles for the OTPVerification Screen

---
+++ src/screens/OTPVerification/OtpVerification.styles.ts
@@ -0,0 +1,92 @@
+import {StyleSheet} from 'react-native';
+import {Theme} from '../../utils/themes';
+
+export const getStyles = (theme: Theme, width: number) =>
+  StyleSheet.create({
+    screenWindow: {
+      flex: 1,
+      backgroundColor: theme.primaryBackground,
+      paddingHorizontal: 24,
+      paddingTop: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    TopView: {
+      alignItems: 'center',
+      marginBottom: 24,
+    },
+    img: {
+      height: 80,
+      width: 80,
+    },
+    EnterOTPText: {
+      fontFamily: 'Nunito-Bold',
+      fontSize: 24,
+      textAlign: 'center',
+      marginTop: 16,
+      color: theme.primaryTextColor,
+    },
+    infoText: {
+      fontFamily: 'Nunito-Regular',
+      fontSize: 14,
+      color: theme.primaryTextColor,
+      textAlign: 'center',
+      marginTop: 8,
+    },
+
+    OtpView: {
+      marginVertical: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    otpContainer: {
+      width: width > 450 ? 'auto' : '100%',
+      gap: width > 450 ? 20 : 20,
+    },
+    pinCodeContainer: {
+      borderColor: 'gray',
+    },
+    pinCodeText: {
+      color: theme.primaryTextColor,
+    },
+    errorText: {
+      color: 'red',
+      fontSize: 13,
+      marginTop: 12,
+      fontFamily: 'Nunito-Regular',
+      textAlign: 'center',
+    },
+    submitView: {
+      alignItems: 'center',
+      marginVertical: 16,
+    },
+    submitViewEnabled: {
+      opacity: 1,
+    },
+    submitViewDisabled: {
+      opacity: 0.5,
+    },
+    bottomView: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 8,
+    },
+    timerText: {
+      fontFamily: 'Nunito-Regular',
+      fontSize: 16,
+      color: theme.primaryTextColor,
+    },
+    resendBtn: {
+      color: '#008080',
+      fontFamily: 'Nunito-Bold',
+      fontSize: 14,
+      marginLeft: 8,
+    },
+    resendBtnDisabled: {
+      opacity: 0.5,
+    },
+    resendBtnEnabled: {
+      opacity: 1,
+    },
+  });
