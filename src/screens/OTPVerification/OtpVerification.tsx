3f4577f8395584f79d53e193f35ca80cb6715d94
Mamatha Niyal <mamathaniyal@Mamathas-MacBook-Pro.local>
2025-06-24 11:53:09 +0530

    Add Otpverification screen along with it's style file

---
+++ src/screens/OTPVerification/OtpVerification.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export const OtpVerification = () =>{
+    return (
+        <>
+        <View>
+            <Text>OTP Verification</Text>
+        </View>
+        </>
+    );
+};
