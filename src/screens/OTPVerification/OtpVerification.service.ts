11a3ce3723d0c7716feb27a2aabf310be4565184
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-26 22:33:09 +0530

    Add verifying login API service

--- src/screens/OTPVerification/OtpVerification.service.ts
+++ src/screens/OTPVerification/OtpVerification.service.ts
@@ -1,3 +1,4 @@
+import { getDeviceId } from 'react-native-device-info';
 import { BASE_URL } from '../../utils/constants';
 
 export const verifyOTP = async (otp: string, mobileNumber: string) => {
@@ -34,4 +35,17 @@ export const createUser = async (mobileNumber: string) => {
     return response;
 
 };
+
+export const verifyLogin = async (mobileNumber: string, otp: string) => {
+    const response = await fetch(`${BASE_URL}verifyLogin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ mobileNumber, deviceId: getDeviceId(), otp }),
+    });
+    return response;
+
+};
+
 
