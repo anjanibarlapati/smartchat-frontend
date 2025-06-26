1fc747ed6a802a3e8d97ba99032d518e70e0dde5
anjanibarlapati <anjani.barlapati@everest.engineering>
2025-06-26 22:34:33 +0530

    Add tests for intergation of OTP verification feature with login

--- src/screens/OTPVerification/OtpVerification.test.tsx
+++ src/screens/OTPVerification/OtpVerification.test.tsx
@@ -7,6 +7,9 @@ import { generateKeyPair, storeKeys } from '../../utils/keyPairs';
 import { decryptPrivateKey, encryptPrivateKey } from '../../utils/privateKey';
 import { OtpVerification } from './OtpVerification';
 import * as OtpService from './OtpVerification.service';
+import * as LoginService from '../Login/Login.service';
+import { storeChats } from '../../realm-database/operations/storeChats';
+import { useRealm } from '../../contexts/RealmContext';
 
 jest.mock('../../hooks/appTheme', () => ({
   useAppTheme: () => ({
@@ -15,6 +18,44 @@ jest.mock('../../hooks/appTheme', () => ({
     textSecondary: '#666',
   }),
 }));
+
+jest.mock('../../utils/keyPairs', () => ({
+  generateKeyPair: jest.fn(),
+  storeKeys: jest.fn(),
+}));
+
+jest.mock('../Login/Login.service', () => ({
+  fetchChats: jest.fn(),
+  formatMessages: jest.fn(),
+}));
+
+jest.mock('../../utils/privateKey', () => ({
+  encryptPrivateKey: jest.fn(),
+  decryptPrivateKey: jest.fn(),
+}));
+
+jest.mock('realm', () => ({
+  BSON: {
+    ObjectId: jest.fn(() => 'mocked-object-id'),
+  },
+}));
+
+jest.mock('../../contexts/RealmContext', () => ({
+  useRealm: jest.fn(),
+}));
+
+jest.mock('../../realm-database/operations/storeChats', () => ({
+  storeChats: jest.fn(),
+}));
+
+jest.mock('../../utils/fcmService', () => ({
+  generateAndUploadFcmToken: jest.fn,
+}));
+
+const mockRealmInstance = {
+  write: jest.fn((fn) => fn()),
+  create: jest.fn(),
+};
 
 jest.mock('./OtpVerification.service');
 
@@ -92,13 +133,19 @@ const mockNavigation = {
   reset: jest.fn(),
 };
 
+let mockRouteParams = {
+  mobileNumber: '1234567890',
+  email: 'test@gmail.com',
+  from: 'registration',
+};
+
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
   return {
     ...actualNav,
     useNavigation: () => mockNavigation,
     useRoute: () => ({
-      params: { mobileNumber: '1234567890', email: 'test@gmail.com' },
+      params: mockRouteParams,
     }),
   };
 });
@@ -114,6 +161,8 @@ const renderOtpVerificationScreen = () => {
 describe('OtpVerification screen tests', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    (useRealm as jest.Mock).mockReturnValue(mockRealmInstance);
+
   });
 
   it('should render static elements', () => {
@@ -132,6 +181,72 @@ describe('OtpVerification screen tests', () => {
     await waitFor(() => {
       expect(getByText('Invalid OTP')).toBeTruthy();
     });
+  });
+
+  it('should countdown timer from 2:00 to 1:59', async () => {
+    jest.useFakeTimers();
+    const { getByText } = renderOtpVerificationScreen();
+    expect(getByText('2:00')).toBeTruthy();
+
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+
+    await waitFor(() => {
+      expect(getByText('1:59')).toBeTruthy();
+    });
+    jest.useRealTimers();
+  });
+
+  it('should not allow submission when OTP is less than 6 digits', async () => {
+    const { getByText, getByLabelText } = renderOtpVerificationScreen();
+    fireEvent.changeText(getByLabelText('One-Time Password'), '1234');
+    const submitButton = getByText('Submit');
+    expect(submitButton.props.accessibilityState?.disabled).toBeFalsy();
+  });
+
+   it('should clear OTP field and hide loader after submission', async () => {
+    (OtpService.verifyOTP as jest.Mock).mockResolvedValue({ ok: true });
+    (OtpService.createUser as jest.Mock).mockResolvedValue({
+      ok: true,
+      json: async () => ({
+        user: { mobileNumber: '1234567890', email: 'test@gmail.com' },
+        userId: 'user123',
+        access_token: 'accessToken',
+        refresh_token: 'refreshToken',
+      }),
+    });
+
+    const { getByText, getByLabelText, queryByLabelText } = renderOtpVerificationScreen();
+    fireEvent.changeText(getByLabelText('One-Time Password'), '123456');
+    fireEvent.press(getByText('Submit'));
+
+    await waitFor(() => {
+      expect(queryByLabelText('One-Time Password')).toBeTruthy();
+    });
+  });
+
+  it('should not allow resend OTP if timer is still running', async () => {
+    const { getByText } = renderOtpVerificationScreen();
+    const resendButton = getByText('Resend Code?');
+    fireEvent.press(resendButton);
+    expect(OtpService.generateOTPAndSendMail).not.toHaveBeenCalled();
+  });
+
+  it('should not resend OTP if already clicked once after timer ends', async () => {
+    jest.useFakeTimers();
+    const { getByText } = renderOtpVerificationScreen();
+
+    act(() => {
+      jest.advanceTimersByTime(120000);
+    });
+
+    fireEvent.press(getByText('Resend Code?'));
+    fireEvent.press(getByText('Resend Code?'));
+    await waitFor(() => {
+      expect(OtpService.generateOTPAndSendMail).toHaveBeenCalledTimes(1);
+    });
+    jest.useRealTimers();
   });
 
   it('should proceed to home screen on successful verification and user creation', async () => {
@@ -265,5 +380,130 @@ describe('OtpVerification screen tests', () => {
       expect(queryByText('Resend failed')).toBeTruthy();
     });
     jest.useRealTimers();
+  });
+
+
+    it('submits OTP and navigates to Tabs for login', async () => {
+    mockRouteParams.from = 'login';
+    const response = {
+      ok: true,
+      json: async () => ({
+        user: {
+          firstName: 'Varun',
+          lastName: 'Kumar',
+          email: 'varun@gmail.com',
+          mobileNumber: '1234567890',
+          privateKey: {salt: 'salt',nonce:'noce', privateKey: 'privateKey'},
+        },
+        userId:'anjani123',
+        access_token: 'access_token',
+        refresh_token: 'refresh_token',
+      }),
+    };
+    const mockChatsResponse = {
+      ok: true,
+      json: async () => ([]),
+    };
+    (decryptPrivateKey as jest.Mock).mockResolvedValue('mockDecryptedPrivateKey');
+
+    (OtpService.verifyLogin as jest.Mock).mockResolvedValue(response);
+    (LoginService.fetchChats as jest.Mock).mockResolvedValue(mockChatsResponse);
+    (LoginService.formatMessages as jest.Mock).mockResolvedValue({});
+    (storeChats as jest.Mock).mockReturnValue({});
+    (EncryptedStorage.setItem as jest.Mock).mockResolvedValue(null);
+
+
+    const { getByText, getByLabelText } = renderOtpVerificationScreen();
+    fireEvent.changeText(getByLabelText('One-Time Password'), '654321');
+    fireEvent.press(getByText('Submit'));
+
+    await waitFor(() => {
+      expect(mockNavigation.reset).toHaveBeenCalledWith({
+        index: 0,
+        routes: [{ name: 'Tabs' }],
+      });
+    });
+  });
+
+  it('shows error if login verification fails', async () => {
+    mockRouteParams.from = 'login';
+
+    (OtpService.verifyLogin as jest.Mock).mockResolvedValue({
+      ok: false,
+      json: async () => ({ message: 'Verification failed' }),
+    });
+
+    const { getByText, getByLabelText } = renderOtpVerificationScreen();
+    fireEvent.changeText(getByLabelText('One-Time Password'), '123456');
+    fireEvent.press(getByText('Submit'));
+
+    await waitFor(() => {
+      expect(getByText('Verification failed')).toBeTruthy();
+    });
+  });
+
+  it('handles thrown error in handleLoginSubmit and navigates to LoginScreen', async () => {
+    mockRouteParams.from = 'login';
+
+    (OtpService.verifyLogin as jest.Mock).mockRejectedValue(new Error('Login API failed'));
+
+    const { getByText, getByLabelText } = renderOtpVerificationScreen();
+    fireEvent.changeText(getByLabelText('One-Time Password'), '123456');
+    fireEvent.press(getByText('Submit'));
+
+    await waitFor(() => {
+      expect(getByText('Login API failed')).toBeTruthy();
+    });
+
+    await act(async () => {
+      await new Promise((r) => setTimeout(r, 3100));
+    });
+
+    expect(mockNavigation.replace).toHaveBeenCalledWith('LoginScreen');
+  });
+
+  it('shows error and navigates to LoginScreen if fetchChats fails', async () => {
+    mockRouteParams.from = 'login';
+
+    const response = {
+      ok: true,
+      json: async () => ({
+        user: {
+          email: 'abc@test.com',
+          mobileNumber: '1234567890',
+          privateKey: {
+            salt: 'salt',
+            nonce: 'nonce',
+            privateKey: 'encryptedKey',
+          },
+        },
+        userId: 'user123',
+        access_token: 'token',
+        refresh_token: 'refresh',
+      }),
+    };
+
+    const chatError = {
+      ok: false,
+      json: async () => ({ message: 'Unable to fetch chats' }),
+    };
+
+    (OtpService.verifyLogin as jest.Mock).mockResolvedValue(response);
+    (LoginService.fetchChats as jest.Mock).mockResolvedValue(chatError);
+    (decryptPrivateKey as jest.Mock).mockResolvedValue('decryptedKey');
+
+    const { getByText, getByLabelText } = renderOtpVerificationScreen();
+    fireEvent.changeText(getByLabelText('One-Time Password'), '654321');
+    fireEvent.press(getByText('Submit'));
+
+    await waitFor(() => {
+      expect(getByText('Unable to fetch chats')).toBeTruthy();
+    });
+
+    await act(async () => {
+      await new Promise((r) => setTimeout(r, 3100));
+    });
+
+    expect(mockNavigation.replace).toHaveBeenCalledWith('LoginScreen');
   });
 });
